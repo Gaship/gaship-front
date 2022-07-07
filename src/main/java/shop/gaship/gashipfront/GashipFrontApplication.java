bb3d0f7b6ddbbe181f9f2e65c32796c37fdd0283
oct-sky-out <kms3335k@naver.com>
2022-07-07 11:18:55 +0900

    first 👑 : gaship-front 프로젝트 생성

---
+++ src/main/java/shop/gaship/gashipfront/GashipFrontApplication.java
@@ -0,0 +1,13 @@
+package shop.gaship.gashipfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GashipFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GashipFrontApplication.class, args);
+    }
+
+}
