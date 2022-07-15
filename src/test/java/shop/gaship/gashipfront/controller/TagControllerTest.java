4ac86796f604fc3432a97365ab49570a64ed5f4c
choijungwoo493 <jwoo1016@gmail.com>
2022-07-15 14:29:43 +0900

    [feat✨] : front Tag test 스켈레톤 추가

---
+++ src/test/java/shop/gaship/gashipfront/controller/TagControllerTest.java
@@ -0,0 +1,44 @@
+package shop.gaship.gashipfront.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : shop.gaship.gashipfront.controller
+ * fileName       : TagControllerTest
+ * author         : choijungwoo
+ * date           : 2022/07/15
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/07/15        choijungwoo       최초 생성
+ */
+class TagControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void registerTag() {
+    }
+
+    @Test
+    void getTag() {
+    }
+
+    @Test
+    void getTags() {
+    }
+
+    @Test
+    void modifyTag() {
+    }
+
+    @Test
+    void deleteTag() {
+    }
+}
