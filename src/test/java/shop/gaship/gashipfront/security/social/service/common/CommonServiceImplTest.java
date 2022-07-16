11e8e719ea7842d8c96cf2a7c4cb1b8e886a3a6a
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-07-16 12:11:23 +0900

    [test🧪] : adapter에서 검증하는 것을 service에서 하기위해서 testcode 먼저 refactoring(검증성공경우, 실패경우)

--- src/test/java/shop/gaship/gashipfront/security/social/service/common/CommonServiceImplTest.java
+++ src/test/java/shop/gaship/gashipfront/security/social/service/common/CommonServiceImplTest.java
@@ -1,6 +1,7 @@
 package shop.gaship.gashipfront.security.social.service.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -14,11 +15,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import shop.gaship.gashipfront.security.social.adapter.Adapter;
 import shop.gaship.gashipfront.security.social.dto.domain.Member;
 import shop.gaship.gashipfront.security.social.dto.jwt.JwtTokenDto;
 import shop.gaship.gashipfront.security.social.dto.jwt.SignInSuccessUserDetailsDto;
+import shop.gaship.gashipfront.security.social.exception.ErrorResponse;
+import shop.gaship.gashipfront.security.social.exception.JwtResponseException;
 
 /**
  * packageName    : shop.gaship.gashipfront.security.social.service.common
@@ -63,7 +70,7 @@ class CommonServiceImplTest {
 
     @DisplayName("식별번호와 권한을 파라미터로 jwt를 요청했고 반환받은 status가 201인경우에 JwtTokenDto타입으로 잘 넘어온다.")
     @Test
-    void getJWT() throws Exception {
+    void getJWT_success() throws Exception {
         // given
         Long identifyNo = 123242124L;
         List<String> authorities = new ArrayList<>();
@@ -72,12 +79,19 @@ class CommonServiceImplTest {
         String accessToken = "this is access token";
         String refreshToken = "this is refresh token";
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
         JwtTokenDto dummyToken = new JwtTokenDto();
         dummyToken.setAccessToken(accessToken);
         dummyToken.setRefreshToken(refreshToken);
 
+        ResponseEntity<Object> response = ResponseEntity.status(201)
+            .headers(headers)
+            .body(dummyToken);
+
         given(adapter.requestJwt(any()))
-            .willReturn(dummyToken);
+            .willReturn(response);
 
         // when
         JwtTokenDto token = commonService.getJWT(identifyNo, authorities);
@@ -87,5 +101,26 @@ class CommonServiceImplTest {
             .isEqualTo(accessToken);
         assertThat(token.getRefreshToken())
             .isEqualTo(refreshToken);
+    }
+
+    @DisplayName("식별번호와 권한을 파라미터로 jwt를 요청했고 반환받은 status가 201이 아닌경우에 JwtResponseException이 발생한다.")
+    @Test
+    void getJWT_fail() throws Exception {
+        // given
+        Long identifyNo = 123242124L;
+        List<String> authorities = new ArrayList<>();
+        authorities.add("USER");
+        
+        ResponseEntity<Object> response = ResponseEntity.status(201)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(new ErrorResponse("auth 서버 오류로 jwt를 생성하지 못했습니다."));
+
+        given(adapter.requestJwt(any()))
+            .willReturn(response);
+
+        // when then
+        assertThatThrownBy(() -> commonService.getJWT(identifyNo, authorities))
+            .isInstanceOf(JwtResponseException.class)
+                .hasMessageContaining("auth 서버");
     }
 }
