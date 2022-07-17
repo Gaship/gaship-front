2d7ed5987f135bb6168dd675a1246b42738bf282
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-07-17 16:47:51 +0900

    [refactor🔨] : 가우스절로 리팩토링

--- src/main/java/shop/gaship/gashipfront/security/social/util/ResponseEntityVerifier.java
+++ src/main/java/shop/gaship/gashipfront/security/social/util/ResponseEntityVerifier.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import shop.gaship.gashipfront.security.social.exception.ErrorResponse;
-import shop.gaship.gashipfront.security.social.exception.ResponseEntityBodyIsErrorResponseException;
 import shop.gaship.gashipfront.security.social.exception.NullResponseBodyException;
+import shop.gaship.gashipfront.security.social.exception.RequestFailureException;
 
 /**
  * ResponseEntity의 응답 body가 예외인지 검증하기 위한 클래스로서 팀원들이 편하게 service단에서 검증할수 있도록 유틸화 시켰습니다.
@@ -15,10 +15,10 @@ import shop.gaship.gashipfront.security.social.exception.NullResponseBodyExcepti
  */
 public class ResponseEntityVerifier {
     public static void verify(ResponseEntity<Object> response, HttpStatus statusCode) {
-        if (!Objects.equals(response.getStatusCode(), statusCode)) {
-            ErrorResponse error = (ErrorResponse) response.getBody();
-            if (Objects.isNull(error)) throw new NullResponseBodyException();
-            throw new ResponseEntityBodyIsErrorResponseException(error.getMessage(), statusCode);
-        }
+        if (Objects.equals(response.getStatusCode(), statusCode)) return;
+
+        ErrorResponse error = (ErrorResponse) response.getBody();
+        if (Objects.isNull(error)) throw new NullResponseBodyException();
+        throw new RequestFailureException(error.getMessage(), statusCode);
     }
 }
