d3056ad48ddeb23153d839534b4a7be9a572ef79
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-07-19 04:20:34 +0900

    [test🧪] : 소셜로그인시 회원이 존재하지 않을경우 회원가입후 로그인기능 테스트추가 및 삭제(oauth2, dance)

--- src/test/java/shop/gaship/gashipfront/security/social/common/service/CommonServiceImplTest.java
+++ src/test/java/shop/gaship/gashipfront/security/social/common/service/CommonServiceImplTest.java
@@ -1,9 +1,83 @@
-import static org.junit.jupiter.api.Assertions.*;
+package shop.gaship.gashipfront.security.social.common.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static reactor.core.publisher.Mono.error;
+import static reactor.core.publisher.Mono.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import shop.gaship.gashipfront.security.social.common.adapter.Adapter;
+import shop.gaship.gashipfront.security.social.common.service.CommonService;
+import shop.gaship.gashipfront.security.social.common.service.CommonServiceImpl;
+import shop.gaship.gashipfront.security.social.member.dto.Member;
+import shop.gaship.gashipfront.security.social.common.dto.Jwt;
+
 /**
- * 
- *  
- * @author : 최겸준 
- * @since 1.0
- */class CommonServiceImplTest {
-  
+ * packageName    : shop.gaship.gashipfront.security.social.service.common
+ * fileName       : ShoppingmallServiceImplTest
+ * author         : choi-gyeom-jun
+ * date           : 2022-07-14
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-14        choi-gyeom-jun       최초 생성
+ */
+
+@ExtendWith(SpringExtension.class)
+@Import(CommonServiceImpl.class)
+class CommonServiceImplTest {
+    @Autowired
+    private CommonService commonService;
+
+    @MockBean
+    private Adapter adapter;
+
+
+    @DisplayName("식별번호와 권한을 파라미터로 jwt를 요청한 경우에 JwtTokenDto타입으로 잘 넘어온다.")
+    @Test
+    void getJWT_success() throws Exception {
+        // given
+        Integer identifyNo = 123242124;
+        List<String> authorities = new ArrayList<>();
+        authorities.add("USER");
+
+        String accessToken = "this is access token";
+        String refreshToken = "this is refresh token";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Jwt dummyToken = new Jwt();
+        dummyToken.setAccessToken(accessToken);
+        dummyToken.setRefreshToken(refreshToken);
+
+        given(adapter.requestJwt(any()))
+            .willReturn(dummyToken);
+        // when
+        Jwt token = commonService.getJWT(identifyNo, authorities);
+
+        // then
+        verify(adapter).requestJwt(any());
+        assertThat(token.getAccessToken())
+            .isEqualTo(accessToken);
+        assertThat(token.getRefreshToken())
+            .isEqualTo(refreshToken);
+    }
 }
--- src/test/java/shop/gaship/gashipfront/security/social/member/service/MemberServiceImplTest.java
+++ src/test/java/shop/gaship/gashipfront/security/social/member/service/MemberServiceImplTest.java
@@ -1,9 +1,102 @@
+package shop.gaship.gashipfront.security.social.member.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import shop.gaship.gashipfront.security.social.common.adapter.Adapter;
+import shop.gaship.gashipfront.security.social.member.dto.Member;
+
 /**
- * 
- *  
- * @author : 최겸준 
+ * @author : 최겸준
  * @since 1.0
- */class MemberServiceImplTest {
-  
+ */
+
+@ExtendWith(SpringExtension.class)
+@Import(MemberServiceImpl.class)
+class MemberServiceImplTest {
+    @Autowired
+    private MemberService memberService;
+
+    @MockBean
+    private Adapter adapter;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @DisplayName("api서버에서 올바른 응답이 온 경우에 더미 member가 잘 반환된다.")
+    @Test
+    void getMemberByEmail() {
+        // given
+        String email = "gbeovhsqhtka@naver.com";
+        Member member = new Member();
+        member.setEmail(email);
+
+        ResponseEntity<Object> response
+            = ResponseEntity.status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(member);
+
+        // mocking
+        given(adapter.requestMemberByEmail(anyString()))
+            .willReturn(member);
+
+        // when
+        Member actualMember = memberService.getMemberByEmail(email);
+
+        // then
+        assertThat(actualMember)
+            .isEqualTo(member);
+    }
+
+    @DisplayName("member가 회원가입하기위한 암호화기능, setter기능, 회원가입요청기능 메서드가 잘 동작한다.")
+    @Test
+    void createMember() {
+        // given
+        Member member = mock(Member.class);
+        given(member.getPassword()).willReturn("zzz");
+        given(passwordEncoder.encode(anyString()))
+            .willReturn("abcd");
+
+        // TODO : 왜쓰는거지
+        doNothing().when(adapter).requestCreateMember(any());
+
+        // when
+        memberService.createMember(member);
+
+        // then
+        verify(passwordEncoder).encode("zzz");
+        verify(member).setPassword("abcd");
+        verify(adapter).requestCreateMember(member);
+    }
+
+    @DisplayName("adapter를 통해 반환된 현재 가입된 마지막 회원번호를 반환한다.")
+    @Test
+    void getLastMemberNo() {
+        // given
+        given(adapter.requestLastMemberNo())
+            .willReturn(1);
+
+        // when
+        Integer memberNo = memberService.getLastMemberNo();
+
+        // then
+        assertThat(memberNo)
+            .isEqualTo(1);
+    }
 }
