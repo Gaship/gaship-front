0220bb912ea342ed919cba19574aef966c18e611
김보민 <qnt012@naver.com>
2022-07-28 10:42:11 +0900

    [feat✨] : 카테고리 어댑터 추가

---
+++ src/main/java/shop/gaship/gashipfront/category/adapter/impl/CategoryAdaptorImpl.java
@@ -0,0 +1,109 @@
+package shop.gaship.gashipfront.category.adapter.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import shop.gaship.gashipfront.category.adapter.CategoryAdapter;
+import shop.gaship.gashipfront.category.dto.request.CategoryCreateRequestDto;
+import shop.gaship.gashipfront.category.dto.request.CategoryModifyRequestDto;
+import shop.gaship.gashipfront.category.dto.response.CategoryResponseDto;
+import shop.gaship.gashipfront.util.ExceptionUtil;
+
+/**
+ * 카테고리 어댑터 구현체입니다.
+ *
+ * @author : 김보민
+ * @see shop.gaship.gashipfront.category.adapter.CategoryAdapter
+ * @since 1.0
+ */
+@Component
+public class CategoryAdaptorImpl implements CategoryAdapter {
+    @Value("${gaship-server.gateway-url}")
+    private String gatewayUrl;
+
+    private static final String REQUEST_URI = "/category";
+
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl(gatewayUrl)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void categoryAdd(CategoryCreateRequestDto createRequest) {
+        webClient.post()
+                .uri(REQUEST_URI)
+                .bodyValue(createRequest)
+                .retrieve()
+                .onStatus(HttpStatus::isError, ExceptionUtil::createErrorMono);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void categoryModify(CategoryModifyRequestDto modifyRequest) {
+        webClient.put()
+                .uri(REQUEST_URI + "/{categoryNo}", modifyRequest.getNo())
+                .bodyValue(modifyRequest)
+                .retrieve()
+                .onStatus(HttpStatus::isError, ExceptionUtil::createErrorMono);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CategoryResponseDto categoryDetails(Integer categoryNo) {
+        return webClient.get()
+                .uri(REQUEST_URI + "/{categoryNo}", categoryNo)
+                .retrieve()
+                .onStatus(HttpStatus::isError, ExceptionUtil::createErrorMono)
+                .bodyToMono(CategoryResponseDto.class)
+                .block();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<CategoryResponseDto> categoryList() {
+        return webClient.get()
+                .uri(REQUEST_URI)
+                .retrieve()
+                .onStatus(HttpStatus::isError, ExceptionUtil::createErrorMono)
+                .bodyToFlux(CategoryResponseDto.class)
+                .collectList()
+                .block();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<CategoryResponseDto> lowerCategoryList(Integer categoryNo) {
+        return webClient.get()
+                .uri(REQUEST_URI + "/{categoryNo}/lower", categoryNo)
+                .retrieve()
+                .onStatus(HttpStatus::isError, ExceptionUtil::createErrorMono)
+                .bodyToFlux(CategoryResponseDto.class)
+                .collectList()
+                .block();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void categoryRemove(Integer categoryNo) {
+        webClient.delete()
+                .uri(REQUEST_URI + "/{categoryNo}", categoryNo)
+                .retrieve()
+                .onStatus(HttpStatus::isError, ExceptionUtil::createErrorMono);
+    }
+}
---
+++ src/main/java/shop/gaship/gashipfront/category/dto/response/CategoryResponseDto.java
@@ -0,0 +1,38 @@
+package shop.gaship.gashipfront.category.dto.response;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 카테고리 조회 응답 시 담기는 데이터 객체입니다.
+ *
+ * @author : 김보민
+ * @since 1.0
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class CategoryResponseDto {
+    private Integer no;
+    private String name;
+    private Integer level;
+    private Integer upperCategoryNo;
+    private String upperCategoryName;
+
+    @Builder
+    public CategoryResponseDto(Integer no,
+                               String name,
+                               Integer level,
+                               Integer upperCategoryNo,
+                               String upperCategoryName) {
+        this.no = no;
+        this.name = name;
+        this.level = level;
+        this.upperCategoryNo = upperCategoryNo;
+        this.upperCategoryName = upperCategoryName;
+    }
+}
