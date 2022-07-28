a81558d0d2288b6ffb66f3b91d5d4f78fabbd19d
HoFe-U <hochul-y@naver.com>
2022-07-28 10:12:01 +0900

    [refactor🔨] : 직원 adaptor 수정

--- src/main/java/shop/gaship/gashipfront/employee/adaptor/EmployeeAdaptor.java
+++ src/main/java/shop/gaship/gashipfront/employee/adaptor/EmployeeAdaptor.java
@@ -1,6 +1,7 @@
 package shop.gaship.gashipfront.employee.adaptor;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -28,7 +29,6 @@ public class EmployeeAdaptor {
     /**
      * @param requestDto 직원생성시 들어갈 정보 입니다.
      * @return boolean 회원가입이 정상적으로 완료시 true 를 반환합니다.
-     * @throws RequestFailureException 네트워크 혹은 웹 클라이언트의 오류를 던집니다.
      * @author 유호철
      */
     public boolean signUpRequest(EmployeeCreateRequestDto requestDto) {
@@ -44,7 +44,6 @@ public class EmployeeAdaptor {
     /**
      * @param requestDto 직원의 정보를 수정할때 들어갈 정보입니다.
      * @return boolean 직원정보가 제대로 수정되었을경우 true 를 반환합니다.
-     * @throws RequestFailureException 네트워크 혹은 웹 클라이언트의 오류를 던집니다.
      * @author 유호철
      */
     public boolean modifyRequest(EmployeeModifyRequestDto requestDto){
@@ -60,7 +59,6 @@ public class EmployeeAdaptor {
     /**
      * @param employeeNo 조회할 직원의 번호 입니다.
      * @return mono 조회된 직원의 정보가 반환됩니다.
-     * @throws RequestFailureException 네트워크 혹은 웹 클라이언트의 오류를 던집니다.
      * @author 유호철
      */
     public Mono<EmployeeResponseDto> getEmployee(Integer employeeNo){
@@ -72,12 +70,14 @@ public class EmployeeAdaptor {
 
     /**
      * @return flux 조회된 직원들의 정보가 반환됩니다.
-     * @throws RequestFailureException 네트워크 혹은 웹 클라이언트의 오류를 던집니다.
      * @author 유호철
      */
-    public Flux<EmployeeResponseDto> getEmployees(){
+    public Flux<EmployeeResponseDto> getEmployees(PageRequest pageRequest){
         return WebClient.create(gatewayBaseurl).get()
-                .uri(EMPLOYEE_BASE_URI)
+                .uri(uriBuilder -> uriBuilder.path(EMPLOYEE_BASE_URI)
+                    .queryParam("size",pageRequest.getPageSize())
+                    .queryParam("page",pageRequest.getPageNumber())
+                    .build())
                 .retrieve()
                 .bodyToFlux(EmployeeResponseDto.class);
     }
--- src/main/java/shop/gaship/gashipfront/employee/dto/response/EmployeeResponseDto.java
+++ src/main/java/shop/gaship/gashipfront/employee/dto/response/EmployeeResponseDto.java
@@ -1,7 +1,11 @@
 package shop.gaship.gashipfront.employee.dto.response;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * 직원의 정보를 받기위한 클래스입니다.
@@ -12,4 +16,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class EmployeeResponseDto {
+
+    @NotNull
+    @Length(min = 1, max = 20)
+    private final String name;
+
+    @Email
+    @NotNull
+    private final String email;
+
+    @NotNull
+    private final String phoneNo;
+
+    @NotBlank
+    private final String address;
 }
