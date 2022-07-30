24fb903dc3203506258fbada665b37c52f3bc0df
choijungwoo493 <jwoo1016@gmail.com>
2022-07-30 16:38:19 +0900

    [test🧪] : 장박구니 컨트롤러 테스트(CUD)

--- src/test/java/shop/gaship/gashipfront/cart/controller/CartControllerTest.java
+++ src/test/java/shop/gaship/gashipfront/cart/controller/CartControllerTest.java
@@ -11,14 +11,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import shop.gaship.gashipfront.cart.dto.request.CartDeleteRequestDto;
+import shop.gaship.gashipfront.cart.dto.request.CartModifyRequestDto;
+import shop.gaship.gashipfront.cart.dto.request.CartProductQuantityUpDownRequestDto;
+import shop.gaship.gashipfront.cart.dto.request.CartRequestDto;
 import shop.gaship.gashipfront.cart.service.CartService;
 import shop.gaship.gashipfront.config.SecurityConfig;
-//import shop.gaship.gashipfront.config.SecurityEmployeeConfig;
+import shop.gaship.gashipfront.config.SecurityEmployeeConfig;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author 최정우
@@ -33,7 +39,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
         },
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class),
-//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityEmployeeConfig.class)
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityEmployeeConfig.class)
         })
 class CartControllerTest {
     @MockBean
@@ -45,19 +51,103 @@ class CartControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @DisplayName("회원이 물건 상세페이지에서 수량을 선택한 후 장바구니에 담기 버튼을 클릭했을 때")
+    @DisplayName("회원이 물건 상세페이지에서 수량을 선택한 후 장바구니에 담기 버튼을 클릭했을 때(Success)")
     @Test
-    void CartAddTest() throws Exception {
-//        doNothing().when(cartService).addToCart(any());
-//
-//        String body = objectMapper.writeValueAsString(CartAddRequestDto.builder().cartId("1").productId("1").carePeriod("10").quantity("1").build());
-//
-//        mockMvc.perform(post("/carts")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .content(body)
-//                    .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isCreated());
-//
-//        verify(cartService,times(1)).addToCart(any());
+    void addToCartTest() throws Exception {
+        doNothing().when(cartService).addProductToCart(any());
+        String body = objectMapper.writeValueAsString(CartRequestDto.builder()
+                .cartId("1")
+                .productId(15)
+                .carePeriod(3)
+                .quantity(2)
+                .build());
+
+        mockMvc.perform(post("/carts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        verify(cartService, times(1)).addProductToCart(any());
+    }
+
+    @Test
+    void modifyFromCartTest() throws Exception{
+        doNothing().when(cartService).modifyProductQuantityFromCart(any());
+        String body = objectMapper.writeValueAsString(CartModifyRequestDto.builder()
+                .cartId("1")
+                .productId(15)
+                .carePeriod(3)
+                .quantity(21)
+                .build());
+
+        mockMvc.perform(put("/carts/1/products/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(cartService, times(1)).modifyProductQuantityFromCart(any());
+    }
+
+    @Test
+    void increaseProductQuantityFromCartTest() throws Exception{
+        doNothing().when(cartService).modifyProductQuantityFromCart(any());
+        String body = objectMapper.writeValueAsString
+                (CartProductQuantityUpDownRequestDto.builder()
+                .cartId("1")
+                .productId(15)
+                .carePeriod(3)
+                .build());
+
+        mockMvc.perform(put("/carts/1/products/1/increase")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(cartService, times(1)).increaseProductQuantityFromCart(any());
+    }
+
+    @Test
+    void decreaseProductQuantityFromCartTest() throws Exception{
+        doNothing().when(cartService).modifyProductQuantityFromCart(any());
+        String body = objectMapper.writeValueAsString
+                (CartProductQuantityUpDownRequestDto.builder()
+                        .cartId("1")
+                        .productId(15)
+                        .carePeriod(3)
+                        .build());
+
+        mockMvc.perform(put("/carts/1/products/1/decrease")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(cartService, times(1)).decreaseProductQuantityFromCart(any());
+    }
+
+    @Test
+    void deleteFromCartTest() throws Exception{
+        doNothing().when(cartService).modifyProductQuantityFromCart(any());
+        String body = objectMapper.writeValueAsString
+                (CartDeleteRequestDto.builder()
+                        .cartId("1")
+                        .productId(15)
+                        .carePeriod(3)
+                        .build());
+
+        mockMvc.perform(delete("/carts/1/products/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(cartService, times(1)).deleteProductFromCart(any());
+    }
+
+    @Test
+    void getProductsFromCartTest() {
     }
 }
