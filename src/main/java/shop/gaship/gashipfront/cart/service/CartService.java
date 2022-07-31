a9acc0c93306e895803368dbcb4aaeedf052e8eb
choijungwoo493 <jwoo1016@gmail.com>
2022-07-31 17:53:07 +0900

    [docs📖] : 장바구니 service 자바독 추가

--- src/main/java/shop/gaship/gashipfront/cart/service/CartService.java
+++ src/main/java/shop/gaship/gashipfront/cart/service/CartService.java
@@ -5,6 +5,9 @@ import shop.gaship.gashipfront.cart.dto.request.CartModifyRequestDto;
 import shop.gaship.gashipfront.cart.dto.request.CartProductQuantityUpDownRequestDto;
 import shop.gaship.gashipfront.cart.dto.request.CartRequestDto;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 장바구니의 service 입니다.
  *
@@ -13,44 +16,45 @@ import shop.gaship.gashipfront.cart.dto.request.CartRequestDto;
  */
 public interface CartService {
     /**
-     * 장바구니에 상품을 추가하는 메서드입니다.
+     * 비회원인 사람이 장바구니에 상품을 추가하는 메서드입니다.
      *
      * @param request 등록에 사용되는 정보 객체입니다.
      */
-    void addProductToCart(CartRequestDto request);
+    void addProductToCart(String cartId, CartRequestDto request);
 
     /**
      * 장바구니에 담긴 상품의 수량을 변경하는 메서드입니다.
      *
      * @param request 변경하려는 대상의 정보가 담기는 객체입니다.
      */
-    void modifyProductQuantityFromCart(CartModifyRequestDto request);
+    void modifyProductQuantityFromCart(String cartId, CartModifyRequestDto request);
 
     /**
      * 장바구니에 담긴 상품의 수량을 +1 메서드입니다.
      *
      * @param request 변경하려는 대상의 정보가 담기는 객체입니다.
      */
-    void increaseProductQuantityFromCart(CartProductQuantityUpDownRequestDto request);
+    void increaseProductQuantityFromCart(String cartId, CartProductQuantityUpDownRequestDto request);
 
     /**
      * 장바구니에 담긴 상품의 수량을 -1 메서드입니다.
      *
      * @param request 변경하려는 대상의 정보가 담기는 객체입니다.
      */
-    void decreaseProductQuantityFromCart(CartProductQuantityUpDownRequestDto request);
+    void decreaseProductQuantityFromCart(String cartId, CartProductQuantityUpDownRequestDto request);
 
     /**
      * 장바구니에서 상품을 삭제하는 메서드입니다.
      *
      * @param request 삭제할 상품의 정보를 담은 객체입니다.
      */
-    void deleteProductFromCart(CartDeleteRequestDto request);
+    void deleteProductFromCart(String cartId, CartDeleteRequestDto request);
 
     /**
      * 장바구니에 담긴 상품들의 정보를 얻는 메서드 입니다.
      *
      * @param cartId 삭제할 상품의 정보를 담은 객체입니다.
+     * @return
      */
-    void getProductsFromCart(String cartId);
+    List<Objects> getProductsFromCart(String cartId);
 }
--- src/main/java/shop/gaship/gashipfront/cart/service/Impl/CartServiceImpl.java
+++ src/main/java/shop/gaship/gashipfront/cart/service/Impl/CartServiceImpl.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import shop.gaship.gashipfront.cart.dto.request.CartDeleteRequestDto;
 import shop.gaship.gashipfront.cart.dto.request.CartModifyRequestDto;
 import shop.gaship.gashipfront.cart.dto.request.CartProductQuantityUpDownRequestDto;
 import shop.gaship.gashipfront.cart.dto.request.CartRequestDto;
+import shop.gaship.gashipfront.cart.exception.IllegalQuantityException;
 import shop.gaship.gashipfront.cart.service.CartService;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
+ * The type Cart service.
+ *
  * @author 최정우
  * @since 1.0
  */
@@ -19,57 +26,112 @@ public class CartServiceImpl implements CartService {
     private final RedisTemplate<String, Object> redisTemplate;
     private final HashOperations<String, String, Integer> hashOperations;
 
-    private static final Integer MINUSONE = -1;
     private static final Integer PLUSONE = 1;
+    private static final Integer MINUSONE = -1;
+
     @Autowired
     public CartServiceImpl(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
         this.hashOperations = redisTemplate.opsForHash();
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * 장바구니에 상품을 추가하는 메서드 입니다.
+     *
+     * @param cartId 장바구니 id 값입니다.
+     * @param request 장바구니에 담을 상품의 정보(상품 id , 보증기간, 갯수)가 담겨 있습니다.
+     */
+    @Transactional
     @Override
-    public void addProductToCart(CartRequestDto request) {
-        String cartKey = request.getCartId();
+    public void addProductToCart(String cartId, CartRequestDto request) {
+        String cartKey = cartId;
+        String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
+        Integer quantity = request.getQuantity();
+
+        hashOperations.increment(cartKey, request.getProductId().toString(), quantity);
+        hashOperations.put(cartKey, hashKey, quantity);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * 장바구니에 담긴 상품의 갯수를 변경하는 메서드 입니다.
+     *
+     * @param cartId 장바구니 id 값입니다.
+     * @param request 장바구니에 담을 상품의 정보(상품 id , 보증기간, 갯수)가 담겨 있습니다.
+     */
+    @Transactional
+    @Override
+    public void modifyProductQuantityFromCart(String cartId, CartModifyRequestDto request) {
+        String cartKey = cartId;
         String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
 
+        Integer originQuantity = hashOperations.get(cartKey, hashKey);
+        if (Objects.isNull(originQuantity)){
+            throw new IllegalQuantityException();
+        }
+        hashOperations.increment(cartKey, request.getProductId().toString(), request.getQuantity()-originQuantity.longValue());
         hashOperations.put(cartKey, hashKey, request.getQuantity());
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * 장바구니에 담긴 상품의 수량을 +1 하는 메서드입니다.
+     *
+     * @param cartId 장바구니 id 값입니다.
+     * @param request 장바구니에 담을 상품의 정보(상품 id , 보증기간)가 담겨 있습니다.
+     */
+    @Transactional
     @Override
-    public void modifyProductQuantityFromCart(CartModifyRequestDto request) {
-        String cartKey = request.getCartId();
+    public void increaseProductQuantityFromCart(String cartId, CartProductQuantityUpDownRequestDto request) {
+        String cartKey = cartId;
         String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
 
-        hashOperations.put(cartKey, hashKey, request.getQuantity());
-    }
-
-    @Override
-    public void increaseProductQuantityFromCart(CartProductQuantityUpDownRequestDto request) {
-        String cartKey = request.getCartId();
-        String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
-
+        hashOperations.increment(cartKey, request.getProductId().toString(), PLUSONE);
         hashOperations.increment(cartKey, hashKey, PLUSONE);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * 장바구니에 담긴 상품의 수량을 -1 하는 메서드입니다.
+     *
+     * @param cartId 장바구니 id 값입니다.
+     * @param request 장바구니에 담을 상품의 정보(상품 id , 보증기간)가 담겨 있습니다.
+     */
+    @Transactional
     @Override
-    public void decreaseProductQuantityFromCart(CartProductQuantityUpDownRequestDto request) {
-        String cartKey = request.getCartId();
+    public void decreaseProductQuantityFromCart(String cartId, CartProductQuantityUpDownRequestDto request) {
+        String cartKey = cartId;
         String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
 
+        hashOperations.increment(cartKey, request.getProductId().toString(), MINUSONE);
         hashOperations.increment(cartKey, hashKey, MINUSONE);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * 장바구니에 담긴 특정 상품을 삭제하는 메서드 입니다.
+     *
+     * @param cartId 장바구니 id 값입니다.
+     * @param request 장바구니에 담을 상품의 정보(상품 id , 보증기간)가 담겨 있습니다.
+     */
+    @Transactional
     @Override
-    public void deleteProductFromCart(CartDeleteRequestDto request) {
-        String cartKey = request.getCartId();
+    public void deleteProductFromCart(String cartId, CartDeleteRequestDto request) {
+        String cartKey = cartId;
         String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
 
+        hashOperations.increment(cartKey, request.getProductId().toString(), MINUSONE);
         hashOperations.delete(cartKey, hashKey);
-
     }
 
     @Override
-    public void getProductsFromCart(String cartId) {
-
+    public List<Objects> getProductsFromCart(String cartId) {
+        return null;
     }
 }
