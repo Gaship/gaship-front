f3e8422df5856d352ab44241bb91ae00f360eb2d
choijungwoo493 <jwoo1016@gmail.com>
2022-08-04 11:05:31 +0900

    [delete🗑] : 어뎁터 삭제

--- src/main/java/shop/gaship/gashipfront/cart/adapter/CartAdapter.java
+++
@@ -1,8 +0,0 @@
-package shop.gaship.gashipfront.cart.adapter;
-
-/**
- * @author 최정우
- * @since 1.0
- */
-public interface CartAdapter {
-}
--- src/main/java/shop/gaship/gashipfront/cart/adopter/CartAdopter.java
+++
@@ -1,8 +0,0 @@
-package shop.gaship.gashipfront.cart.adopter;
-
-/**
- * @author 최정우
- * @since 1.0
- */
-public interface CartAdopter {
-}
