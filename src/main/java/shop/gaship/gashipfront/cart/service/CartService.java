632513e3379ce0232edf217195150e533452ece2
choijungwoo493 <jwoo1016@gmail.com>
2022-08-04 11:12:02 +0900

    [docs📖] : 자바독 수정

--- src/main/java/shop/gaship/gashipfront/cart/service/CartService.java
+++ src/main/java/shop/gaship/gashipfront/cart/service/CartService.java
@@ -6,18 +6,16 @@ import shop.gaship.gashipfront.cart.dto.request.CartProductQuantityUpDownRequest
 import shop.gaship.gashipfront.cart.dto.request.CartRequestDto;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * 장바구니의 service 입니다.
- *
  * @author 최정우
  * @since 1.0
  */
 public interface CartService {
     /**
      * 비회원인 사람이 장바구니에 상품을 추가하는 메서드입니다.
-     *
+     * @param cartId  장바구니 id 값입니다.
      * @param request 등록에 사용되는 정보 객체입니다.
      * @author 최정우
      */
@@ -25,15 +23,15 @@ public interface CartService {
 
     /**
      * 장바구니에 담긴 상품의 수량을 변경하는 메서드입니다.
-     *
-     * @param request 변경하려는 대상의 정보가 담기는 객체입니다.
+     * @param cartId  장바구니 id 값입니다.
+     * @param request 장바구니에 담을 상품의 정보(상품 id , 보증기간, 갯수)가 담겨 있습니다.
      * @author 최정우
      */
     void modifyProductQuantityFromCart(String cartId, CartModifyRequestDto request) throws Exception;
 
     /**
      * 장바구니에 담긴 상품의 수량을 +1 메서드입니다.
-     *
+     *@param cartId  장바구니 id 값입니다.
      * @param request 변경하려는 대상의 정보가 담기는 객체입니다.
      * @author 최정우
      */
@@ -41,7 +39,7 @@ public interface CartService {
 
     /**
      * 장바구니에 담긴 상품의 수량을 -1 메서드입니다.
-     *
+     * @param cartId 수량을 변경하려는 카트의 아이디 입니다.
      * @param request 변경하려는 대상의 정보가 담기는 객체입니다.
      * @author 최정우
      */
@@ -49,7 +47,7 @@ public interface CartService {
 
     /**
      * 장바구니에서 상품을 삭제하는 메서드입니다.
-     *
+     * @param cartId  장바구니 id 값입니다.
      * @param request 삭제할 상품의 정보를 담은 객체입니다.
      * @author 최정우
      */
@@ -57,19 +55,18 @@ public interface CartService {
 
     /**
      * 비회원의 장바구니 상품 목록을 회원의 장바구니에 옮기는 메서드 입니다.
-     *
      * @param nonMemberCartId 비회원일 때의 장바구니 id
      * @param memberId        회원일 때의 장바구니 id
+     * @author 최정우
      */
     void mergeCart(String nonMemberCartId, Integer memberId);
 
     /**
      * 장바구니에 담긴 상품들의 정보를 얻는 메서드 입니다.
-     *
      * @param cartId 삭제할 상품의 정보를 담은 객체입니다.
      * @return
      * @author 최정우
      */
-    List<Objects> getProductsFromCart(String cartId);
+    List<Integer> getProductsFromCart(String cartId);
 
 }
