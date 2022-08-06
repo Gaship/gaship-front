470fa559147021778582fb5b5e52cadbff56fde9
choijungwoo493 <jwoo1016@gmail.com>
2022-08-07 02:07:38 +0900

    [feat✨] : AddressListController 개발

--- src/main/java/shop/gaship/gashipfront/addresslist/controller/AddressListController.java
+++ src/main/java/shop/gaship/gashipfront/addresslist/controller/AddressListController.java
@@ -1,8 +1,96 @@
 package shop.gaship.gashipfront.addresslist.controller;
 
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import shop.gaship.gashipfront.addresslist.dto.request.AddressListAddRequestDto;
+import shop.gaship.gashipfront.addresslist.dto.request.AddressListModifyRequestDto;
+import shop.gaship.gashipfront.addresslist.service.AddressListService;
+
 /**
+ * 배송지목록의 컨트롤러입니다.
+ *
  * @author 최정우
  * @since 1.0
  */
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("member/{memberNo}/address-list")
 public class AddressListController {
+    private static final String DEFAULT_PATH = "addressLists";
+    private final AddressListService addressListService;
+
+    /**
+     * 배송지목록을 등록하고 배송지목록으로 돌아갑니다.
+     *
+     * @param request 배송지목록등록에 쓰이는 정보입니다.
+     * @return 배송지목록 목록을 보여주는 페이지
+     * @author 최정우
+     */
+    @PostMapping
+    public String addressListAdd(@RequestBody @Valid AddressListAddRequestDto request) {
+        addressListService.addAddressList(request);
+
+        return "redirect:/" + DEFAULT_PATH;
+    }
+
+    /**
+     * 배송지목록을 수정하고 배송지목록으로 돌아갑니다.
+     *
+     * @param request 배송지목록수정에 쓰이는 정보입니다.
+     * @return 배송지목록 목록을 보여주는 페이지
+     * @author 최정우
+     */
+    @PutMapping("/{addressListNo}")
+    public String addressListModify(@RequestBody @Valid AddressListModifyRequestDto request) {
+        addressListService.modifyAddressList(request);
+        return "redirect:" + DEFAULT_PATH;
+    }
+
+    /**
+     * 배송지목록을 수정하고 배송지목록으로 돌아갑니다.
+     *
+     * @param addressListNo 수정할 배송지목록의 id 입니다.
+     * @return 배송지목록 목록을 보여주는 페이지
+     * @author 최정우
+     */
+    @DeleteMapping("/{addressListNo}")
+    public String addressListRemove(@PathVariable Long addressListNo) {
+        addressListService.deleteAddressList(addressListNo);
+        return "redirect:" + DEFAULT_PATH;
+    }
+
+    /**
+     * 배송지목록의 상세 페이지 입니다.
+     *
+     * @param addressListNo 조회하려는 배송지목록의 id 입니다.
+     * @return 배송지목록 상세페이지
+     * @author 최정우
+     */
+    @GetMapping("/{addressListNo}")
+    @ResponseBody
+    public String addressListDetails(@PathVariable Long addressListNo,
+                                     Model model) {
+        model.addAttribute("response", addressListService.findAddressList(addressListNo));
+        return "addressListInfo";
+    }
+
+    /**
+     * 배송지목록을 수정하고 배송지목록으로 돌아갑니다.
+     *
+     * @param pageable 조회하려는 페이지 번호와 사이즈가 담겨져있는 객체
+     * @param model    모델
+     * @return 배송지목록 목록을 보여주는 페이지
+     * @author 최정우
+     */
+    @GetMapping
+    @ResponseBody
+    public String addressLists(Pageable pageable,
+                               Model model) {
+        model.addAttribute("response", addressListService.findAddressLists(pageable));
+        return DEFAULT_PATH;
+    }
 }
--- src/main/java/shop/gaship/gashipfront/addresslist/service/AddressListService.java
+++ src/main/java/shop/gaship/gashipfront/addresslist/service/AddressListService.java
@@ -1,8 +1,21 @@
 package shop.gaship.gashipfront.addresslist.service;
 
+import org.springframework.data.domain.Pageable;
+import shop.gaship.gashipfront.addresslist.dto.request.AddressListAddRequestDto;
+import shop.gaship.gashipfront.addresslist.dto.request.AddressListModifyRequestDto;
+
 /**
  * @author 최정우
  * @since 1.0
  */
 public interface AddressListService {
+    void addAddressList(AddressListAddRequestDto request);
+
+    void modifyAddressList(AddressListModifyRequestDto request);
+
+    void deleteAddressList(Long addressListNo);
+
+    Object findAddressList(Long addressListNo);
+
+    Object findAddressLists(Pageable pageable);
 }
