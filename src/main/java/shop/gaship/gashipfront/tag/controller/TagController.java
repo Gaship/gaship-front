314ffce8f5e6773b5f379580b3f5ffe5ef71b927
choijungwoo493 <jwoo1016@gmail.com>
2022-08-06 13:42:46 +0900

    [refactor🔨] : 태그 컨트롤러 반환값 변경,  자바독 수정

--- src/main/java/shop/gaship/gashipfront/tag/controller/TagController.java
+++ src/main/java/shop/gaship/gashipfront/tag/controller/TagController.java
@@ -1,112 +1,84 @@
 package shop.gaship.gashipfront.tag.controller;
 
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import shop.gaship.gashipfront.dto.*;
+import shop.gaship.gashipfront.tag.dto.request.TagAddRequestDto;
+import shop.gaship.gashipfront.tag.dto.request.TagModifyRequestDto;
 import shop.gaship.gashipfront.tag.service.TagService;
-import shop.gaship.gashipfront.tag.dto.*;
 
 /**
- * The type Tag controller.
+ * 태그 관련 요청을 처리합니다.
+ *
+ * @author 최정우
+ * @since 1.0
  */
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/admins/{adminId}/tags")
+@RequestMapping("/tags")
 public class TagController {
+    private static final String DEFAULT_PATH = "/tags";
     private final TagService tagService;
 
     /**
-     * Tag add model and view.
+     * 태그를 등록하고 태그목록으로 돌아갑니다.
      *
-     * @param tagRegisterRequestDto the tag register request dto
-     * @param adminId               the admin id
-     * @param modelAndView          the model and view
-     * @return the model and view
+     * @param request 태그등록에 쓰이는 정보입니다.
+     * @return 태그 목록을 보여주는 페이지
+     * @author 최정우
      */
     @PostMapping
-    public ModelAndView tagAdd(@ModelAttribute TagRegisterRequestDto tagRegisterRequestDto,
-                                    @PathVariable Integer adminId,
-                                    ModelAndView modelAndView) {
-        tagService.addTag(adminId, tagRegisterRequestDto);
-        modelAndView.setViewName("redirect:/admins/" + adminId + "/tags");
-        return modelAndView;
+    public String tagAdd(@RequestBody @Valid TagAddRequestDto request) {
+        tagService.addTag(request);
+
+        return "redirect:" + DEFAULT_PATH;
     }
 
-
     /**
-     * Tag modify model and view.
+     * 태그를 수정하고 태그목록으로 돌아갑니다.
      *
-     * @param tagModifyRequestDto the tag modify request dto
-     * @param adminId             the admin id
-     * @param modelAndView        the model and view
-     * @return the model and view
+     * @param request 태그수정에 쓰이는 정보입니다.
+     * @return 태그 목록을 보여주는 페이지
+     * @author 최정우
      */
-    @PutMapping("/{tagId}")
-    public ModelAndView tagModify(@ModelAttribute TagModifyRequestDto tagModifyRequestDto,
-                                  @PathVariable Integer adminId,
-                                  ModelAndView modelAndView) {
-        tagService.modifyTag(adminId, tagModifyRequestDto);
-        modelAndView.setViewName("redirect:/admins/" + adminId + "/tags/" + tagModifyRequestDto.getTagId() + "details");
-        return modelAndView;
+    @PutMapping("/{tagNo}")
+    public String tagModify(@RequestBody @Valid TagModifyRequestDto request) {
+        tagService.modifyTag(request);
+        return "redirect:" + DEFAULT_PATH;
     }
 
     /**
-     * Tag remove model and view.
+     * 태그를 수정하고 태그목록으로 돌아갑니다.
      *
-     * @param tagDeleteRequestDto the tag delete request dto
-     * @param adminId             the admin id
-     * @param modelAndView        the model and view
-     * @return the model and view
+     * @param tagNo 조회하려는 태그의 id 입니다.
+     * @return 태그 수정 페이지
+     * @author 최정우
      */
-    @DeleteMapping("/{tagId}")
-    public ModelAndView tagRemove(@ModelAttribute TagDeleteRequestDto tagDeleteRequestDto,
-                                  @PathVariable Integer adminId,
-                                  ModelAndView modelAndView) {
-        tagService.removeTag(adminId, tagDeleteRequestDto);
-        modelAndView.setViewName("redirect:/admins/" + adminId + "/tags");
-        return modelAndView;
+    @GetMapping("/{tagNo}")
+    @ResponseBody
+    public String tagDetails(@PathVariable Long tagNo,
+                             Model model) {
+        model.addAttribute("response", tagService.findTag(tagNo));
+
+        return "tagInfo";
     }
 
     /**
-     * Tag details model and view.
+     * 태그를 수정하고 태그목록으로 돌아갑니다.
      *
-     * @param tagGetRequestDto the tag get request dto
-     * @param adminId          the admin id
-     * @param modelAndView     the model and view
-     * @return the model and view
-     */
-    @GetMapping("/{tagId}")
-    public ModelAndView tagDetails(@ModelAttribute TagGetRequestDto tagGetRequestDto,
-                               @PathVariable Integer adminId,
-                               ModelAndView modelAndView) {
-        Mono<TagResponseDto> tag = tagService.findTag(adminId, tagGetRequestDto);
-        modelAndView.addObject(tag);
-        modelAndView.setViewName("tagInfo");
-        return modelAndView;
-    }
-
-    /**
-     * Tag list model and view.
-     *
-     * @param tagGetRequestDto the tag get request dto
-     * @param adminId          the admin id
-     * @param modelAndView     the model and view
-     * @param pageable         the pageable
-     * @return the model and view
+     * @param pageable 조회하려는 페이지 번호와 사이즈가 담겨져있는 객체
+     * @param model    모델
+     * @return 태그 목록을 보여주는 페이지
+     * @author 최정우
      */
     @GetMapping
-    public ModelAndView tagList(@ModelAttribute TagGetRequestDto tagGetRequestDto,
-                                @PathVariable Integer adminId,
-                                ModelAndView modelAndView,
-                                Pageable pageable) {
-        Flux<TagResponseDto> tags = tagService.findTags(adminId, tagGetRequestDto, pageable);
-        modelAndView.addObject(tags);
-        modelAndView.setViewName("redirect:/admins/" + adminId + "/tags");
-        return modelAndView;
+    @ResponseBody
+    public String tagList(Pageable pageable,
+                          Model model) {
+        model.addAttribute("response", tagService.findTags(pageable));
+        return DEFAULT_PATH;
     }
 }
