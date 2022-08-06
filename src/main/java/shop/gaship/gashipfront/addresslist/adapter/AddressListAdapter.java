7b537da45caf6ee43807929866e859776934e759
choijungwoo493 <jwoo1016@gmail.com>
2022-08-07 03:00:21 +0900

    [feat✨] : 배송지목록 서비스 매개변수 변경에 따른 어뎁터 매개변수 변경

--- src/main/java/shop/gaship/gashipfront/addresslist/adapter/AddressListAdapter.java
+++ src/main/java/shop/gaship/gashipfront/addresslist/adapter/AddressListAdapter.java
@@ -33,25 +33,27 @@ public interface AddressListAdapter {
     /**
      * 배송지목록를 삭제(상태변경)하는 어뎁터입니다.
      *
+     * @param memberNo 삭제하려는 배송지목록의 회원 id 입니다.
      * @param addressListNo 삭제하려는 배송지목록의 id 입니다.
      * @author 최정우
      */
-    void deleteAddressList(Long addressListNo);
+    void deleteAddressList(Long memberNo, Long addressListNo);
 
     /**
      * 배송지목록을 상세조회하는 어뎁터입니다.
      *
+     * @param memberNo 조회하려는 배송지목록의 회원 id 입니다.
      * @param addressListNo 조회하려는 배송지의 id 입니다.
      * @author 최정우
      */
-    AddressListResponseDto findAddressList(Long addressListNo);
+    AddressListResponseDto findAddressList(Long memberNo, Long addressListNo);
 
     /**
      * 배송지목록을 다건조회 어뎁터입니다.
      *
-     * @param memberNo 배송지목록을 조회하려는 대상 회원의 id 입니다.
+     * @param memberNo 조회하려는 배송지목록의 회원 id 입니다.
      * @param pageable 조회하려는 페이지의 번호와 사이즈가 있습니다.
      * @author 최정우
      */
-    PageResponse<AddressListResponseDto> findAddressLists(String memberNo, Pageable pageable);
+    PageResponse<AddressListResponseDto> findAddressLists(Long memberNo, Pageable pageable);
 }
