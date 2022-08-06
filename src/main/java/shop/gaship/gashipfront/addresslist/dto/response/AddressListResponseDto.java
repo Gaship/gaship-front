6a90b8d7290d0834dcf669e1f4d378be212ea7ae
choijungwoo493 <jwoo1016@gmail.com>
2022-08-07 02:57:47 +0900

    [feat✨] : 배송지목록 response dto 구현

--- src/main/java/shop/gaship/gashipfront/addresslist/dto/response/AddressListResponseDto.java
+++ src/main/java/shop/gaship/gashipfront/addresslist/dto/response/AddressListResponseDto.java
@@ -1,8 +1,17 @@
 package shop.gaship.gashipfront.addresslist.dto.response;
 
+import lombok.Getter;
+
 /**
  * @author 최정우
  * @since 1.0
  */
+@Getter
 public class AddressListResponseDto {
+    private Integer addressListNo;
+    private String addressName;
+    private boolean allowDelivery;
+    private String address;
+    private String addressDetail;
+    private String zipCode;
 }
