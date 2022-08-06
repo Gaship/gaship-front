634bf7464d00322cd6bcf99bbcf66fa75bfbcf76
choijungwoo493 <jwoo1016@gmail.com>
2022-08-07 02:21:15 +0900

    [feat✨] : 배송지목록 어뎁터 인터페이스 구현

--- src/main/java/shop/gaship/gashipfront/addresslist/adapter/AddressListAdapter.java
+++ src/main/java/shop/gaship/gashipfront/addresslist/adapter/AddressListAdapter.java
@@ -1,8 +1,23 @@
 package shop.gaship.gashipfront.addresslist.adapter;
 
+import org.springframework.data.domain.Pageable;
+import shop.gaship.gashipfront.addresslist.dto.request.AddressListAddRequestDto;
+import shop.gaship.gashipfront.addresslist.dto.request.AddressListModifyRequestDto;
+import shop.gaship.gashipfront.addresslist.dto.response.AddressListResponseDto;
+import shop.gaship.gashipfront.util.dto.PageResponse;
+
 /**
  * @author 최정우
  * @since 1.0
  */
 public interface AddressListAdapter {
+    void addAddressList(AddressListAddRequestDto request);
+
+    void modifyAddressList(AddressListModifyRequestDto request);
+
+    void deleteAddressList(Long addressListNo);
+
+    AddressListResponseDto findAddressList(Long addressListNo);
+
+    PageResponse<AddressListResponseDto> findAddressLists(String memberNo, Pageable pageable);
 }
