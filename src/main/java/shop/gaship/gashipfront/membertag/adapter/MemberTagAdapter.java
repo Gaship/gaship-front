e454e76a9d96160d298438325b334052929083c3
choijungwoo493 <jwoo1016@gmail.com>
2022-08-10 11:13:08 +0900

    [docs📖] : 자바독 수정

--- src/main/java/shop/gaship/gashipfront/membertag/adapter/MemberTagAdapter.java
+++ src/main/java/shop/gaship/gashipfront/membertag/adapter/MemberTagAdapter.java
@@ -1,9 +1,9 @@
 package shop.gaship.gashipfront.membertag.adapter;
 
+import java.util.List;
 import shop.gaship.gashipfront.membertag.dto.request.MemberTagRequestDto;
 import shop.gaship.gashipfront.membertag.dto.response.MemberTagResponseDto;
 
-import java.util.List;
 
 /**
  * 회원 태그 어뎁터 인터페이스.
@@ -13,14 +13,14 @@ import java.util.List;
  */
 public interface MemberTagAdapter {
     /**
-     * 회원이 설정한 모든 태그를 삭제하고 회원이 설정하길 원하는 태그를 등록하는 메서드 입니다.
+     * 회원이 설정한 모든 태그를 삭제하고 회원이 설정하길 원하는 태그를 등록하는 메서드.
      *
      * @param request 회원 id, 태그 id 리스트 가 담겨있는 객체
      */
     void deleteAllAndAddAllMemberTags(MemberTagRequestDto request);
 
     /**
-     * 회원이 설정한 모든 태그를 가져오는 메서드
+     * 회원이 설정한 모든 태그를 가져오는 메서드.
      *
      * @param memberNo 조회하고자 하는 대상의 id
      */
