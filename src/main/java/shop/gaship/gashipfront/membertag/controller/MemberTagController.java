b39ef7a88bef384c717781c1436051631b085b7d
choijungwoo493 <jwoo1016@gmail.com>
2022-08-10 10:55:13 +0900

    [style🎨] : 코드 포맷팅(RedirectAttribute 추가, 페이지 이름 변경)

--- src/main/java/shop/gaship/gashipfront/membertag/controller/MemberTagController.java
+++ src/main/java/shop/gaship/gashipfront/membertag/controller/MemberTagController.java
@@ -1,19 +1,16 @@
 package shop.gaship.gashipfront.membertag.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import shop.gaship.gashipfront.membertag.dto.request.MemberTagRequestDto;
-import shop.gaship.gashipfront.membertag.dto.response.MemberTagResponseDto;
+import shop.gaship.gashipfront.membertag.dto.response.MemberTagCoreResponseDto;
 import shop.gaship.gashipfront.membertag.service.MemberTagService;
-import shop.gaship.gashipfront.tag.dto.request.TagAddRequestDto;
-import shop.gaship.gashipfront.tag.dto.request.TagModifyRequestDto;
-import shop.gaship.gashipfront.tag.service.TagService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * 회원태그 관련 요청을 처리합니다.
@@ -23,15 +20,15 @@ import javax.validation.Valid;
  */
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("members/{memberNo}/tags")
+@RequestMapping("members/{memberNo}/member-tag")
 public class MemberTagController {
     private final MemberTagService memberTagService;
 
     /**
      * 자신의 태그를 설정하고 태그목록 리스트화면으로 돌아갑니다.
      *
-     * @param memberNo 조회대상 회원의 id
-     * @param request 등록된 태그들의 id 와 회원의 id 가 있습니다.
+     * @param memberNo           조회대상 회원의 id
+     * @param request            등록된 태그들의 id 와 회원의 id 가 있습니다.
      * @param redirectAttributes redirectAttributes
      * @return 태그 목록을 보여주는 컨트롤러 주소
      * @author 최정우
@@ -42,10 +39,10 @@ public class MemberTagController {
             @RequestBody @Valid MemberTagRequestDto request,
             RedirectAttributes redirectAttributes) {
         memberTagService.deleteAllAndAddAllMemberTags(request);
-        redirectAttributes.addAttribute("memberNo",memberNo);
-        redirectAttributes.addAttribute("status",true);
+        redirectAttributes.addAttribute("memberNo", memberNo);
+        redirectAttributes.addAttribute("status", true);
 
-        return "redirect:/members/{memberNo}/tags";
+        return "redirect:/members/{memberNo}/member-tag";
     }
 
     /**
@@ -59,8 +56,9 @@ public class MemberTagController {
     @GetMapping
     public String memberTagList(@PathVariable Integer memberNo,
                                 Model model) {
-        model.addAttribute("response", memberTagService.findMemberTags(memberNo));
+        List<MemberTagCoreResponseDto> memberTags = memberTagService.findMemberTags(memberNo);
+        model.addAttribute("response", memberTags);
 
-        return "basic/memberTag";
+        return "membertag";
     }
 }
