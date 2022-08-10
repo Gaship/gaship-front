9bb1433f9ad2b8e569fd224563e67e3ee607a639
choijungwoo493 <jwoo1016@gmail.com>
2022-08-10 11:12:15 +0900

    [docs📖] : 자바독 수정

--- src/main/java/shop/gaship/gashipfront/membertag/dto/response/MemberTagCoreResponseDto.java
+++ src/main/java/shop/gaship/gashipfront/membertag/dto/response/MemberTagCoreResponseDto.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 
 /**
+ * 화면에 반환하기 위한 dto.
+ *
  * @author 최정우
  * @since 1.0
  */
@@ -13,6 +15,13 @@ public class MemberTagCoreResponseDto {
     private final String title;
     private boolean selected;
 
+    /**
+     * 모든 태그 리스트와 회원이 선택한 태그들을 반환하는 dto를 만들기 위한 Builder.
+     *
+     * @param tagNo 태그 id
+     * @param title 태그명
+     * @param selected 회원이 태그를 선택한 여부
+     */
     @Builder
     public MemberTagCoreResponseDto(Integer tagNo, String title, boolean selected) {
         this.tagNo = tagNo;
@@ -20,7 +29,7 @@ public class MemberTagCoreResponseDto {
         this.selected = selected;
     }
 
-    public void setSelectedTrue(){
+    public void setSelectedTrue() {
         this.selected = true;
     }
 }
