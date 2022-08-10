fd737f7a718fc58a3ed1d4ce7d56f7b13d54c7c3
choijungwoo493 <jwoo1016@gmail.com>
2022-08-10 10:57:00 +0900

    [feat✨] : 화면에 보여줄 정보가 담겨있는 dto 에 정보를 매핑하는 코드 추가

--- src/main/java/shop/gaship/gashipfront/membertag/service/Impl/MemberTagServiceImpl.java
+++ src/main/java/shop/gaship/gashipfront/membertag/service/Impl/MemberTagServiceImpl.java
@@ -2,15 +2,18 @@ package shop.gaship.gashipfront.membertag.service.Impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import shop.gaship.gashipfront.member.adapter.MemberAdapter;
 import shop.gaship.gashipfront.membertag.adapter.MemberTagAdapter;
 import shop.gaship.gashipfront.membertag.dto.request.MemberTagRequestDto;
+import shop.gaship.gashipfront.membertag.dto.response.MemberTagCoreResponseDto;
 import shop.gaship.gashipfront.membertag.dto.response.MemberTagResponseDto;
 import shop.gaship.gashipfront.membertag.service.MemberTagService;
 import shop.gaship.gashipfront.tag.adapter.TagAdapter;
 import shop.gaship.gashipfront.tag.dto.response.TagResponseDto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 회원태그 서비스 구현체.
@@ -36,9 +39,25 @@ public class MemberTagServiceImpl implements MemberTagService {
      * {@inheritDoc}
      */
     @Override
-    public MemberTagResponseDto findMemberTags(Integer memberNo) {
+    public List<MemberTagCoreResponseDto> findMemberTags(Integer memberNo) {
         List<MemberTagResponseDto> dtoList = memberTagAdapter.findMemberTags(memberNo);
         List<TagResponseDto> tags = tagAdapter.findTags();
-        return null;
+        List<Integer> tagNoList = dtoList.stream().map(MemberTagResponseDto::getTagNo).collect(Collectors.toList());
+        List<MemberTagCoreResponseDto> responseDtoList = new ArrayList<>();
+        tags.forEach(ele ->
+                responseDtoList.add(MemberTagCoreResponseDto.builder()
+                        .tagNo(ele.getTagNo())
+                        .title(ele.getTitle())
+                        .selected(false)
+                        .build()));
+        for (Integer targetId : tagNoList) {
+            responseDtoList.forEach(dto ->
+            {
+                if (Objects.equals(dto.getTagNo(), targetId)) {
+                    dto.setSelectedTrue();
+                }
+            });
+        }
+        return responseDtoList;
     }
 }
