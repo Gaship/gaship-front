ee168ee3303d09fc990d33c1b4fd3fb33a4b18c7
choijungwoo493 <jwoo1016@gmail.com>
2022-08-11 23:56:12 +0900

    [feat✨] : 반환값 dto 설정

---
+++ src/main/java/shop/gaship/gashipfront/member/dto/response/MemberResponseDto.java
@@ -0,0 +1,35 @@
+package shop.gaship.gashipfront.member.dto.response;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 멤버정보 반환값을 담는 dto 입니다.
+ *
+ * @author 최정우
+ * @since 1.0
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberResponseDto {
+    private Integer memberNo;
+    private String recommendMemberName;
+    private String memberStatus;
+    private String memberGrade;
+    private String email;
+    private List<String> authorities;
+    private String phoneNumber;
+    private String nickname;
+    private String name;
+    private String gender;
+    private LocalDate birthDate;
+    private Long accumulatePurchaseAmount;
+    private LocalDate nextRenewalGradeDate;
+    private LocalDateTime registerDatetime;
+}
