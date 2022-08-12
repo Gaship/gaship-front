2d455fce5ffd319a7f634ed9b9e15874090f8ba3
choijungwoo493 <jwoo1016@gmail.com>
2022-08-12 16:17:42 +0900

    [refactor🔨] : 장바구니 정책 변경에 따른 dto 리팩토링

--- src/main/java/shop/gaship/gashipfront/cart/dto/request/CartProductDeleteRequestDto.java
+++ src/main/java/shop/gaship/gashipfront/cart/dto/request/CartProductDeleteRequestDto.java
@@ -1,18 +1,17 @@
 package shop.gaship.gashipfront.cart.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 /**
+ * 장바구니에 담긴 물건을 삭제하는 정보를 담은 dto 입니다.
+ *
  * @author 최정우
  * @since 1.0
  */
 @Getter
-@Builder
-public class CartProductQuantityUpDownRequestDto {
+public class CartProductDeleteRequestDto {
     @Min(value = 1, message = "회원 id 는 1 이상이어야합니다.")
     private Integer userId;
 
--- src/main/java/shop/gaship/gashipfront/cart/dto/request/CartProductModifyRequestDto.java
+++ src/main/java/shop/gaship/gashipfront/cart/dto/request/CartProductModifyRequestDto.java
@@ -1,21 +1,25 @@
 package shop.gaship.gashipfront.cart.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 /**
+ * 장바구니에 담긴 물품의 수량변경정보를 담은 dto 입니다.
+ *
  * @author 최정우
  * @since 1.0
  */
 @Getter
-@Builder
-public class CartProductQuantityUpDownRequestDto {
+public class CartProductModifyRequestDto {
     @Min(value = 1, message = "회원 id 는 1 이상이어야합니다.")
     private Integer userId;
 
     @Min(value = 1, message = "상품 id 는 1 이상이어야합니다.")
     private Integer productId;
+
+    @Min(value = 1, message = "상품은 한개 이상부터 주문가응합니다.")
+    @Max(value = 10, message = "상품은 한 번에 열개 까지 구매가능합니다.")
+    private Integer quantity;
 }
