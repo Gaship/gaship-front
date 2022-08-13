c6219b232aac76bb3dae3b06f5eb23874e78b10c
choijungwoo493 <jwoo1016@gmail.com>
2022-08-13 15:44:08 +0900

    [refactor🔨] : 쿠키값 조회로직 변경에 따른 리팩토링

--- src/main/java/shop/gaship/gashipfront/cart/controller/CartController.java
+++ src/main/java/shop/gaship/gashipfront/cart/controller/CartController.java
@@ -1,22 +1,27 @@
 package shop.gaship.gashipfront.cart.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import shop.gaship.gashipfront.cart.dto.request.CartProductAddRequestDto;
-import shop.gaship.gashipfront.cart.dto.request.CartProductDeleteRequestDto;
-import shop.gaship.gashipfront.cart.dto.request.CartProductModifyRequestDto;
-import shop.gaship.gashipfront.cart.dto.request.CartProductQuantityUpDownRequestDto;
-import shop.gaship.gashipfront.cart.service.CartService;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 import java.util.UUID;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import shop.gaship.gashipfront.cart.dto.request.CartProductDeleteRequestDto;
+import shop.gaship.gashipfront.cart.dto.request.CartProductModifyRequestDto;
+import shop.gaship.gashipfront.cart.exception.CartProductAmountException;
+import shop.gaship.gashipfront.cart.service.CartService;
+
+
 
 /**
  * 장바구니를 구현하기 위한 컨트롤러 입니다.
@@ -28,156 +33,86 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("/carts")
 public class CartController {
-    private static final String NON_MEMBER_CART_ID = "NMCARTID";
-    private static final String MEMBER_CART_ID = "MCARTID";
+    private static final String CART_ID = "CID";
     private final CartService cartService;
-    private String cartId;
 
     /**
      * 상품상세 페이지에서 보증기간과 수량을 선택하고 장바구니에 물건을 추가하는 컨트롤러입니다.
-     * 쿠키의 비회원, 회원 장바구니 존재 여부가 분기문 조건입니다.
      * 쿠키 id 값을 키로, 장바구니에 물건을 추가합니다.
      *
-     * @param request         상품Id, 보증기간, 수량이 들어있습니다.
-     * @param nonMemberCartId 비회원 장바구니의 id 값 입니다.
-     * @param memberCartId    회원 장바구니의 id 값 입니다.
+     * @param request 상품Id, 보증기간, 수량이 들어있습니다.
+     * @param cartId  장바구니 id
      * @return 반환값은 Void 타입의 ResponseEntity 입니다.
      * @author 최정우
      */
-    @PostMapping
-    public String addToCart(@RequestBody CartProductAddRequestDto request,
-                            @CookieValue(value = NON_MEMBER_CART_ID, required = false) String nonMemberCartId,
-                            @CookieValue(value = MEMBER_CART_ID, required = false) String memberCartId,
-                            HttpServletResponse response,
-                            RedirectAttributes attributes) {
-        cartId = assignCartId(nonMemberCartId, memberCartId);
+    @PostMapping("/add-product")
+    @ResponseBody
+    public Integer addToCart(@RequestBody CartProductModifyRequestDto request,
+                             @CookieValue(value = CART_ID, required = false) String cartId,
+                             HttpServletResponse response,
+                             RedirectAttributes attributes) throws CartProductAmountException {
         if (Objects.isNull(cartId)) {
-            cartId = UUID.randomUUID().toString();
-            response.addCookie(new Cookie(NON_MEMBER_CART_ID, cartId));
+            String newCartId = UUID.randomUUID().toString();
+            response.addCookie(new Cookie(CART_ID, newCartId));
+            cartId = newCartId;
         }
-        cartService.addProductToCart(cartId, request);
-        attributes.addAttribute("status",true);
-
-        return "redirect:/cart";
+        attributes.addAttribute("status", true);
+        return cartService.modifyProductQuantityFromCart(cartId, request);
     }
 
     /**
      * 장바구니 페이지에서 수량을 변경하는 컨트롤러입니다.
      *
-     * @param request         상품을 식별하기 위한 id 값과 보증기간 그리고 변경하길 원하는 제품의 최종 수량이 들어있습니다.
-     * @param nonMemberCartId 비회원 장바구니의 id 값 입니다.
-     * @param memberCartId    회원 장바구니의 id 값 입니다.
-     * @return 반환값은 Void 타입의 ResponseEntity 입니다.
+     * @param request 상품 id 값, 상품수량이 들어있는 객체
+     * @param cartId  장바구니 id
      * @author 최정우
      */
-    @PutMapping("/products")
-    public ResponseEntity<Void> modifyFromCart(@RequestBody CartProductModifyRequestDto request,
-                                               @CookieValue(value = NON_MEMBER_CART_ID, required = false) String nonMemberCartId,
-                                               @CookieValue(value = MEMBER_CART_ID, required = false) String memberCartId) throws Exception {
-        cartId = assignCartId(nonMemberCartId, memberCartId);
-        cartService.modifyProductQuantityFromCart(cartId, request);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .build();
-    }
-
-    /**
-     * 장바구니 페이지에서 상품의 수량을  +1 하는 컨트롤러입니다.
-     *
-     * @param request         cartId 와 상품을 식별하기 위한 id 값과 보증기간이 있는 객체입니다.
-     * @param nonMemberCartId 비회원 장바구니의 id 값 입니다.
-     * @param memberCartId    회원 장바구니의 id 값 입니다.
-     * @return 반환값은 Void 타입의 ResponseEntity 입니다.
-     * @author 최정우
-     */
-    @PutMapping("/products/increase")
-    public ResponseEntity<Void> increaseProductQuantityFromCart(
-            @RequestBody CartProductQuantityUpDownRequestDto request,
-            @CookieValue(value = NON_MEMBER_CART_ID, required = false) String nonMemberCartId,
-            @CookieValue(value = MEMBER_CART_ID, required = false) String memberCartId) {
-        cartId = assignCartId(nonMemberCartId, memberCartId);
-        cartService.increaseProductQuantityFromCart(cartId, request);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .build();
-    }
-
-    /**
-     * 장바구니 페이지에서 상품의 수량을 -1 하는 컨트롤러입니다.
-     *
-     * @param request         cartId 와 상품을 식별하기 위한 id 값과 보증기간이 있는 객체입니다.
-     * @param nonMemberCartId 비회원 장바구니의 id 값 입니다.
-     * @param memberCartId    회원 장바구니의 id 값 입니다.
-     * @return 반환값은 Void 타입의 ResponseEntity 입니다.
-     * @author 최정우
-     */
-    @PutMapping("/products/decrease")
-    public ResponseEntity<Void> decreaseProductQuantityFromCart(@RequestBody CartProductQuantityUpDownRequestDto request,
-                                                                @CookieValue(value = NON_MEMBER_CART_ID, required = false) String nonMemberCartId,
-                                                                @CookieValue(value = MEMBER_CART_ID, required = false) String memberCartId) throws Exception {
-        cartId = assignCartId(nonMemberCartId, memberCartId);
-        cartService.decreaseProductQuantityFromCart(cartId, request);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .build();
+    @PutMapping("/modify-quantity")
+    @ResponseBody
+    public Integer modifyFromCart(
+            @RequestBody CartProductModifyRequestDto request,
+            @CookieValue(value = CART_ID) String cartId)
+            throws CartProductAmountException {
+        return cartService.modifyProductQuantityFromCart(cartId, request);
     }
 
     /**
      * 장바구니 페이지에서 해당 상품을 삭제하는 컨트롤러 입니다.
      *
-     * @param request         cartId 와 상품을 식별하기 위한 id 값과 보증기간이 있는 객체입니다.
-     * @param nonMemberCartId 비회원 장바구니의 id 값 입니다.
-     * @param memberCartId    회원 장바구니의 id 값 입니다.
+     * @param request 상품 id 값
+     * @param cartId  장바구니 id
      * @return 반환값은 Void 타입의 ResponseEntity 입니다.
      * @author 최정우
      */
-    @DeleteMapping("/products/delete")
-    public ResponseEntity<Void> deleteFromCart(@RequestBody CartProductDeleteRequestDto request,
-                                               @CookieValue(value = NON_MEMBER_CART_ID, required = false) String nonMemberCartId,
-                                               @CookieValue(value = MEMBER_CART_ID, required = false) String memberCartId) throws Exception {
-        cartId = assignCartId(nonMemberCartId, memberCartId);
+    @DeleteMapping("/delete-product")
+    @ResponseBody
+    public boolean deleteFromCart(
+            @RequestBody CartProductDeleteRequestDto request,
+            @CookieValue(value = CART_ID) String cartId) throws CartProductAmountException {
         cartService.deleteProductFromCart(cartId, request);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .build();
+        return true;
     }
 
     /**
      * 장바구니를 보여주는 컨트롤러 입니다.
      *
-     * @param nonMemberCartId 비회원 장바구니의 id 값 입니다.
-     * @param memberCartId    회원 장바구니의 id 값 입니다.
-     * @param response        리스폰스 입니다.
-     * @param model           model 입니다.
-     * @return 장바수니 html 을 반환합니다.
+     * @param cartId   장바구니의 id 값 입니다.
+     * @param response 리스폰스 입니다.
+     * @param model    model 입니다.
+     * @return 장바구니 페이지 목록페이지
      * @author 최정우
      */
     @GetMapping
-    public String getProductsFromCart(@CookieValue(value = NON_MEMBER_CART_ID, required = false) String nonMemberCartId,
-                                      @CookieValue(value = MEMBER_CART_ID, required = false) String memberCartId,
+    public String getProductsFromCart(@CookieValue(value = CART_ID, required = false) String cartId,
                                       HttpServletResponse response,
                                       Model model) {
-        cartId = assignCartId(nonMemberCartId, memberCartId);
         if (Objects.isNull(cartId)) {
-            cartId = UUID.randomUUID().toString();
-            response.addCookie(new Cookie(NON_MEMBER_CART_ID, cartId));
+            String newCartId = UUID.randomUUID().toString();
+            response.addCookie(new Cookie(CART_ID, newCartId));
+            return "carts";
         }
         model.addAttribute(cartService.getProductsFromCart(cartId));
 
         return "carts";
-    }
-
-    private String assignCartId(String nonMemberCartId, String memberCartId) {
-        if (Objects.isNull(nonMemberCartId) && Objects.isNull(memberCartId)) {
-            return null;
-        } else if (Objects.nonNull(nonMemberCartId)) {
-            cartId = nonMemberCartId;
-        } else {
-            cartId = memberCartId;
-        }
-        return cartId;
     }
 }
