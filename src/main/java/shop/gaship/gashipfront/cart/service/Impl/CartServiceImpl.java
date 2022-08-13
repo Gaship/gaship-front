e86e03f6253912b3cd6714d9910564446b50e961
choijungwoo493 <jwoo1016@gmail.com>
2022-08-13 15:45:35 +0900

    [refactor🔨] : 쿠키값 조회로직 변경에 따른 리팩토링

--- src/main/java/shop/gaship/gashipfront/cart/service/Impl/CartServiceImpl.java
+++ src/main/java/shop/gaship/gashipfront/cart/service/Impl/CartServiceImpl.java
@@ -1,5 +1,7 @@
 package shop.gaship.gashipfront.cart.service.Impl;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,16 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.gaship.gashipfront.cart.dto.request.CartProductDeleteRequestDto;
 import shop.gaship.gashipfront.cart.dto.request.CartProductModifyRequestDto;
-import shop.gaship.gashipfront.cart.dto.request.CartProductQuantityUpDownRequestDto;
 import shop.gaship.gashipfront.cart.exception.CartProductAmountException;
-import shop.gaship.gashipfront.cart.exception.IllegalQuantityException;
 import shop.gaship.gashipfront.cart.service.CartService;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * {@inheritDoc}
@@ -24,10 +19,8 @@ import java.util.stream.Collectors;
  */
 @Service
 public class CartServiceImpl implements CartService {
-    private static final Integer PLUSONE = 1;
-    private static final Integer MINUSONE = -1;
     private final RedisTemplate<String, Object> redisTemplate;
-    private final HashOperations<String, String, Integer> hashOperations;
+    private final HashOperations<String, Integer, Integer> hashOperations;
 
     @Autowired
     public CartServiceImpl(RedisTemplate<String, Object> redisTemplate) {
@@ -40,85 +33,30 @@ public class CartServiceImpl implements CartService {
      */
     @Transactional
     @Override
-    public void addProductToCart(String cartId, CartRequestDto request) {
-        String cartKey = cartId;
-        String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
-        Integer quantity = request.getQuantity();
-
-        hashOperations.increment(cartKey, hashKey, quantity);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Transactional
-    @Override
-    public void modifyProductQuantityFromCart(String cartId, CartProductModifyRequestDto request) {
-        String cartKey = cartId;
-        String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
-
-        hashOperations.put(cartKey, hashKey, request.getQuantity());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Transactional
-    @Override
-    public void increaseProductQuantityFromCart(String cartId, CartProductQuantityUpDownRequestDto request) {
-        String cartKey = cartId;
-        String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
-
-        hashOperations.increment(cartKey, hashKey, PLUSONE);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @throws CartProductAmountException 담겨있는 상품의 갯수가 1이하이면 발생하는 오류
-     */
-    @Transactional
-    @Override
-    public void decreaseProductQuantityFromCart(String cartId, CartProductQuantityUpDownRequestDto request) throws CartProductAmountException {
-        String cartKey = cartId;
-        String hashKey = request.getProductId().toString() + "-" + request.getCarePeriod().toString();
-        Integer cartProductAmount = hashOperations.get(cartKey, hashKey);
-
-        if (cartProductAmount <= 1) {
+    public Integer modifyProductQuantityFromCart(
+            String cartNo, CartProductModifyRequestDto request) throws CartProductAmountException {
+        if (request.getQuantity() > 10 || request.getQuantity() < 1) {
             throw new CartProductAmountException();
         }
-        hashOperations.increment(cartKey, hashKey, MINUSONE);
+        hashOperations.put(cartNo, request.getProductId(), request.getQuantity());
+        return request.getQuantity();
     }
 
     /**
      * {@inheritDoc}
-     *
-     * @throws IllegalQuantityException 조회한 상품이 null 이면 오류
      */
     @Transactional
     @Override
-    public void deleteProductFromCart(String cartId, CartProductDeleteRequestDto request) throws Exception {
-        String cartKey = cartId;
-        String productId = request.getProductId().toString();
-        String hashKey = productId + "-" + request.getCarePeriod().toString();
-
-        hashOperations.increment(cartKey, hashKey, MINUSONE);
-        Integer productAndCarePeriodQuantity = hashOperations.get(cartKey, hashKey);
-        if (Objects.isNull(productAndCarePeriodQuantity)) {
-            throw new IllegalQuantityException();
-        }
-        if (productAndCarePeriodQuantity < 1) {
-            hashOperations.delete(cartKey, hashKey);
-        }
+    public void deleteProductFromCart(String cartNo, CartProductDeleteRequestDto request) {
+        hashOperations.delete(cartNo, request.getProductId());
     }
 
     /**
      * {@inheritDoc}
      */
-
     @Override
     public void mergeCart(String nonMemberCartId, Integer memberId) {
-        Map<String, Integer> map = hashOperations.entries(nonMemberCartId);
+        Map<Integer, Integer> map = hashOperations.entries(nonMemberCartId);
         String key = String.valueOf(memberId);
         mergeHashMap(key, map);
         hashOperations.delete(nonMemberCartId);
@@ -127,24 +65,14 @@ public class CartServiceImpl implements CartService {
     /**
      * 키 값이 다른 두개의 레디스를 합치는 메서드 입니다.
      */
-    private void mergeHashMap(String key, Map<String, Integer> map) {
+    private void mergeHashMap(String key, Map<Integer, Integer> map) {
         map.forEach((key1, value) -> hashOperations.putIfAbsent(key, key1, value));
     }
 
     @Override
     public List<Integer> getProductsFromCart(String cartId) {
-        return getDistinctIntProductId(cartId);
+        Map<Integer, Integer> products = hashOperations.entries(cartId);
+        return null;
     }
 
-    private List<Integer> getDistinctIntProductId(String cartId) {
-        Set<String> keys = hashOperations.keys(cartId);
-        return keys.stream()
-                .map(key -> key.substring(0, key.lastIndexOf("-")))
-                .distinct()
-                .map(Integer::valueOf)
-                .collect(Collectors.toList());
-
-
-
-    }
 }
