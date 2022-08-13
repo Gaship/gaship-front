ee1a193d627b38f11e42bceff10a097982ebaa43
choijungwoo493 <jwoo1016@gmail.com>
2022-08-13 15:19:27 +0900

    [refactor🔨] : 예외처리 수정 및 삭제

--- src/main/java/shop/gaship/gashipfront/cart/exception/CartProductAmountException.java
+++ src/main/java/shop/gaship/gashipfront/cart/exception/CartProductAmountException.java
@@ -1,11 +1,13 @@
 package shop.gaship.gashipfront.cart.exception;
 
 /**
+ * 특정 상품의 수량이 10인데 수량 + 버튼을 누른 경우.
+ *
  * @author 최정우
  * @since 1.0
  */
 public class CartProductAmountException extends Exception {
-    public static final String MESSAGE = "장바구니에 담긴 상품의 갯수는 0 이하가 될 수 없습니다.";
+    public static final String MESSAGE = "상품의 수량은 1~10개 사이로 선택해주십시오.";
 
     public CartProductAmountException() {
         super(MESSAGE);
