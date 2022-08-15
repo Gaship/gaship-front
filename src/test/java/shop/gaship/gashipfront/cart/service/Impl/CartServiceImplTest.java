497d3a5fc4536d31f5a199384ed1b7b13caf4733
choijungwoo493 <jwoo1016@gmail.com>
2022-08-15 12:29:21 +0900

    [refactor🔨] : 장바구니 조회를 페이지에서 리스트로 정책 변경에 따른 서비스 테스트 수정

--- src/test/java/shop/gaship/gashipfront/cart/service/Impl/CartServiceImplTest.java
+++ src/test/java/shop/gaship/gashipfront/cart/service/Impl/CartServiceImplTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import shop.gaship.gashipfront.cart.dummy.CartDummy;
 import shop.gaship.gashipfront.cart.exception.CartProductAmountException;
 import shop.gaship.gashipfront.cart.service.CartService;
+import shop.gaship.gashipfront.product.adapter.ProductAdapter;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -36,6 +37,9 @@ class CartServiceImplTest {
     @Autowired
     private CartService cartService;
 
+    @MockBean
+    ProductAdapter productAdapter;
+
     @BeforeEach
     void setUp() {
         when(redisTemplate.opsForHash()).thenReturn(hashOperations);
@@ -49,18 +53,18 @@ class CartServiceImplTest {
 
         cartService.modifyProductQuantityFromCart("1", CartDummy.cartProductModifyRequestDto(1, 1));
 
-        verify(hashOperations, times(1)).put("1", 1, 1);
+        verify(hashOperations, times(1)).put("1", "1", 1);
     }
 
     @DisplayName("장바구니 상품수량 변경 테스트(수량이 10 개 초과(Fail))")
     @Test
-    void modifyProductQuantityFromCartTestFail() throws CartProductAmountException {
+    void modifyProductQuantityFromCartTestFail() {
         doNothing().when(hashOperations).put(any(), any(), any());
 
         assertThatThrownBy(()-> cartService.modifyProductQuantityFromCart("1", CartDummy.cartProductModifyRequestDto(1, 11)))
                 .isInstanceOf(CartProductAmountException.class);
 
-        verify(hashOperations, never()).put("1", 1, 1);
+        verify(hashOperations, never()).put("1", "1", 1);
     }
 
     @DisplayName("장바구니 상품수량 변경 테스트(수량이 1 개 미만(Fail))")
@@ -71,7 +75,7 @@ class CartServiceImplTest {
         assertThatThrownBy(()-> cartService.modifyProductQuantityFromCart("1", CartDummy.cartProductModifyRequestDto(1, 0)))
                 .isInstanceOf(CartProductAmountException.class);
 
-        verify(hashOperations, never()).put("1", 1, 1);
+        verify(hashOperations, never()).put("1", "1", 1);
     }
 
     @DisplayName("장바구니 특정 상품 삭제 테스트")
@@ -81,12 +85,13 @@ class CartServiceImplTest {
 
         cartService.deleteProductFromCart("1", CartDummy.cartProductDeleteRequestDto(1));
 
-        verify(hashOperations, times(1)).delete("1", 1);
+        verify(hashOperations, times(1)).delete("1", "1");
     }
 
     @DisplayName("장바구니 조회 테스트")
     @Test
     void getProductsFromCartTest() {
+
     }
 
 
