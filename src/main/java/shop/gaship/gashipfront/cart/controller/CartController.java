ae71438572d98d03e0c927b350626c7fa79d1e20
choijungwoo493 <jwoo1016@gmail.com>
2022-08-16 13:52:03 +0900

    [test🧪] : 장바구니에 담기는 수량 조정

--- src/main/java/shop/gaship/gashipfront/cart/controller/CartController.java
+++ src/main/java/shop/gaship/gashipfront/cart/controller/CartController.java
@@ -56,8 +56,12 @@ public class CartController {
             response.addCookie(new Cookie(CART_ID, newCartId));
             cartId = newCartId;
         }
+        Integer orderQuantity = cartService.modifyProductQuantityFromCart(cartId, request);
+        if (orderQuantity == 0) {
+            attributes.addAttribute("cartMax", true);
+        }
         attributes.addAttribute("status", true);
-        return cartService.modifyProductQuantityFromCart(cartId, request);
+        return orderQuantity;
     }
 
     /**
