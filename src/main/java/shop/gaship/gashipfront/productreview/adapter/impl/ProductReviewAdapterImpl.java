3253bef140c3726219642cc21b9da0cd8e913618
김보민 <qnt012@naver.com>
2022-08-19 16:38:00 +0900

    [feat✨] : 상품평 어댑터 작성

---
+++ src/main/java/shop/gaship/gashipfront/productreview/adapter/impl/ProductReviewAdapterImpl.java
@@ -0,0 +1,61 @@
+package shop.gaship.gashipfront.productreview.adapter.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import shop.gaship.gashipfront.productreview.adapter.ProductReviewAdapter;
+import shop.gaship.gashipfront.productreview.dto.response.ProductReviewResponseDto;
+import shop.gaship.gashipfront.response.PageResponse;
+
+/**
+ * 상품평 어댑터 구현체입니다.
+ *
+ * @author : 김보민
+ * @see shop.gaship.gashipfront.productreview.adapter.ProductReviewAdapter
+ * @since 1.0
+ */
+@Component
+@RequiredArgsConstructor
+public class ProductReviewAdapterImpl implements ProductReviewAdapter {
+    private final WebClient webClient;
+
+    @Override
+    public ProductReviewResponseDto productReviewDetails(Integer orderProductNo) {
+        return webClient.get()
+                .uri("/api/reviews/{orderProdcutNo}", orderProductNo)
+                .retrieve()
+                .bodyToMono(ProductReviewResponseDto.class)
+                .block();
+    }
+
+    @Override
+    public PageResponse<ProductReviewResponseDto> productReviewList() {
+        return webClient.get()
+                .uri("/api/reviews")
+                .retrieve()
+                .bodyToMono(
+                        new ParameterizedTypeReference<PageResponse<ProductReviewResponseDto>>() {
+                }).block();
+    }
+
+    @Override
+    public PageResponse<ProductReviewResponseDto> productReviewListByProduct(Integer productNo) {
+        return webClient.get()
+                .uri("/api/products/{productNo}/reviews", productNo)
+                .retrieve()
+                .bodyToMono(
+                        new ParameterizedTypeReference<PageResponse<ProductReviewResponseDto>>() {})
+                .block();
+    }
+
+    @Override
+    public PageResponse<ProductReviewResponseDto> productReviewListByMember(Integer memberNo) {
+        return webClient.get()
+                .uri("/api/members/{memberNo}/reviews", memberNo)
+                .retrieve()
+                .bodyToMono(
+                        new ParameterizedTypeReference<PageResponse<ProductReviewResponseDto>>() {})
+                .block();
+    }
+}
---
+++ src/main/java/shop/gaship/gashipfront/productreview/dto/response/ProductReviewResponseDto.java
@@ -0,0 +1,26 @@
+package shop.gaship.gashipfront.productreview.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 상품평 조회 응답 dto입니다.
+ *
+ * @author : 김보민
+ * @since 1.0
+ */
+@Getter
+@NoArgsConstructor
+public class ProductReviewResponseDto {
+    private Integer orderProductNo;
+    private String writerNickname;
+    private String productName;
+    private String title;
+    private String content;
+    private Integer starScore;
+    private LocalDateTime registerDateTime;
+    private LocalDateTime modifyDateTime;
+    private List<Integer> fileNos;
+}
