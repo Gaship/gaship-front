1ca19286ae1a2b55316f8e96540260850edbf0fb
oct-sky-out <kms3335k@naver.com>
2022-08-23 01:26:42 +0900

    [doc📖] : SignUpDenyException showAdminPage javadoc 추가,

--- src/main/java/shop/gaship/gashipfront/home/ManagerHomeController.java
+++ src/main/java/shop/gaship/gashipfront/home/ManagerHomeController.java
@@ -8,16 +8,24 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 설명작성란
+ * 매니저, 관리자 홈 컨트롤러 클래스입니다.
  *
  * @author 김민수
  * @since 1.0
  */
 @Controller
+@RequestMapping("/manager")
 public class ManagerHomeController {
-    @GetMapping("/manager")
+    /**
+     * 사용자가 현재 관리자 혹은 직원의 권한이라면 관리자화면을 아니라면 관리자 로그인화면으로
+     * 
+     * @param principal securityContextHolder에 저장된 사용자의 정보입니다.
+     * @return 사용자 주체가 없으면 루트로, 권한이 없다면 관리자 로그인으로, 권한이 있다면 관리자 페이지를 반환합니다.
+     */
+    @GetMapping
     public String showAdminPage(@AuthenticationPrincipal UserDetails principal){
         if(Objects.isNull(principal)){
             return "redirect:/";
@@ -27,7 +35,7 @@ public class ManagerHomeController {
         SimpleGrantedAuthority userAuthority = new SimpleGrantedAuthority("ROLE_USER");
 
         if(principal.getAuthorities().isEmpty() || authorities.contains(userAuthority)) {
-            return "login/adminLogin";
+            return "redirect:/manager/login";
         }
 
         return "adminIndex";
--- src/main/java/shop/gaship/gashipfront/member/exception/SignUpDenyException.java
+++ src/main/java/shop/gaship/gashipfront/member/exception/SignUpDenyException.java
@@ -1,7 +1,7 @@
 package shop.gaship.gashipfront.member.exception;
 
 /**
- * 설명작성란
+ * 회원가입 거부발생시 일어나는 예외입니다.
  *
  * @author 김민수
  * @since 1.0
