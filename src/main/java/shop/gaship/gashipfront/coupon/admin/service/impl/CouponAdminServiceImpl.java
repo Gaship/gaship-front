161d9612468dca7f6026194067a459e4a7022c51
조재철 <rhsnqk@gmail.com>
2022-08-25 10:37:18 +0900

    [feat✨] : 관리자 페이지 쿠폰 view 구현

---
+++ src/main/java/shop/gaship/gashipfront/coupon/admin/service/impl/CouponAdminServiceImpl.java
@@ -0,0 +1,85 @@
+package shop.gaship.gashipfront.coupon.admin.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import shop.gaship.gashipfront.coupon.admin.adapter.CouponAdminAdapter;
+import shop.gaship.gashipfront.coupon.admin.service.CouponAdminService;
+import shop.gaship.gashipfront.coupon.dto.CouponTypeDto;
+import shop.gaship.gashipfront.coupon.dto.CouponTypeCreationDto;
+import shop.gaship.gashipfront.util.dto.PageResponse;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+@Service
+@RequiredArgsConstructor
+public class CouponAdminServiceImpl implements CouponAdminService {
+
+    private final CouponAdminAdapter couponAdminAdapter;
+
+    @Override
+    public void addCouponTypeFixedAmount(CouponTypeCreationDto couponTypeCreationDto) {
+        couponAdminAdapter.addFixedAmountCouponType(couponTypeCreationDto);
+    }
+
+    @Override
+    public void addCouponTypeFixedRate(CouponTypeCreationDto couponTypeCreationDto) {
+        couponAdminAdapter.addFixedRateCouponType(couponTypeCreationDto);
+    }
+
+    @Override
+    public void modifyRecommendMemberCoupon(CouponTypeCreationDto couponTypeCreationDto) {
+        couponAdminAdapter.modifyRecommendMemberCoupon(couponTypeCreationDto);
+    }
+
+    @Override
+    public void modifyCouponTypeStopGenerationIssue(Integer couponTypeNo) {
+        couponAdminAdapter.modifyCouponTypeStopGenerationIssue(couponTypeNo);
+    }
+
+    @Override
+    public void deleteCouponType(Integer couponTypeNo) {
+        couponAdminAdapter.deleteCouponType(couponTypeNo);
+    }
+
+    @Override
+    public PageResponse<CouponTypeDto> findCouponTypeList(Pageable pageable) {
+        PageResponse<CouponTypeDto> couponTypeList = couponAdminAdapter.findCouponTypeList(pageable);
+
+        return couponTypeList;
+    }
+
+    @Override
+    public PageResponse<CouponTypeDto> findCouponTypeCanDeleteList(Pageable pageable) {
+        PageResponse<CouponTypeDto> couponTypeCanDeleteList =
+            couponAdminAdapter.findCouponTypeCanDeleteList(pageable);
+
+        return couponTypeCanDeleteList;
+    }
+
+    @Override
+    public PageResponse<CouponTypeDto> findCouponTypeCannotDeleteList(Pageable pageable) {
+        PageResponse<CouponTypeDto> couponTypeCannotDeleteList =
+            couponAdminAdapter.findCouponTypeCannotDeleteList(pageable);
+
+        return couponTypeCannotDeleteList;
+    }
+
+    @Override
+    public PageResponse<CouponTypeDto> findCouponTypeFixedAmountList(Pageable pageable) {
+        PageResponse<CouponTypeDto> couponTypeFixedAmountList =
+            couponAdminAdapter.findCouponTypeFixedAmountList(pageable);
+
+        return couponTypeFixedAmountList;
+    }
+
+    @Override
+    public PageResponse<CouponTypeDto> findCouponTypeFixedRateList(Pageable pageable) {
+        PageResponse<CouponTypeDto> couponTypeFixedRateList =
+            couponAdminAdapter.findCouponTypeFixedRateList(pageable);
+
+        return couponTypeFixedRateList;
+    }
+}
---
+++ src/main/java/shop/gaship/gashipfront/coupon/member/controller/CouponMemberController.java
@@ -0,0 +1,9 @@
+package shop.gaship.gashipfront.coupon.member.controller;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+public class CouponMemberController {
+
+}
