3c773fb7237d0fd202d89f7a97450b1965412daf
김보민 <qnt012@naver.com>
2022-08-25 14:09:47 +0900

    [feat✨] : pagination 추가

--- src/main/java/shop/gaship/gashipfront/productreview/controller/ProductReviewController.java
+++ src/main/java/shop/gaship/gashipfront/productreview/controller/ProductReviewController.java
@@ -1,9 +1,12 @@
 package shop.gaship.gashipfront.productreview.controller;
 
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 import shop.gaship.gashipfront.productreview.dto.request.ProductReviewRequestDto;
 import shop.gaship.gashipfront.productreview.service.ProductReviewService;
+import shop.gaship.gashipfront.security.common.dto.UserDetailsDto;
 
 /**
  * 상품평 컨트롤러입니다.
@@ -68,8 +72,13 @@ public class ProductReviewController {
 
     @GetMapping("/products/{productNo}/reviews")
     public String getProductReviews(@PathVariable Integer productNo,
-                                    Pageable pageable,
-                                    Model model) {
+                                    @PageableDefault(size = 5) Pageable pageable,
+                                    Model model,
+                                    @AuthenticationPrincipal UserDetailsDto userDetailsDto) {
+        if (Objects.nonNull(userDetailsDto)) {
+            model.addAttribute("memberNickname",
+                    userDetailsDto.getMember().getNickName());
+        }
         model.addAttribute("reviews",
                 productReviewService.findReviewsByProduct(productNo, pageable));
 
@@ -78,8 +87,13 @@ public class ProductReviewController {
 
     @GetMapping("/members/{memberNo}/reviews")
     public String getMemberReviews(@PathVariable Integer memberNo,
-                                   Pageable pageable,
-                                   Model model) {
+                                   @PageableDefault(size = 5) Pageable pageable,
+                                   Model model,
+                                   @AuthenticationPrincipal UserDetailsDto userDetailsDto) {
+        if (Objects.nonNull(userDetailsDto)) {
+            model.addAttribute("memberNickname",
+                    userDetailsDto.getMember().getNickName());
+        }
         model.addAttribute("reviews",
                 productReviewService.findReviewsByMember(memberNo, pageable));
 
