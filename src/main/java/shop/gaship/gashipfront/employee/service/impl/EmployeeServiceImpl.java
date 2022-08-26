adaa2494f725c1b33039609e51ddb3f3e960f982
HoFe-U <hochul-y@naver.com>
2022-08-26 18:17:25 +0900

    [feat✨] : 관리자 직원리스트 틀구현

---
+++ src/main/java/shop/gaship/gashipfront/employee/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package shop.gaship.gashipfront.employee.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import shop.gaship.gashipfront.employee.adapter.impl.EmployeeAdapterImpl;
+import shop.gaship.gashipfront.employee.dto.request.EmployeeCreateRequestDto;
+import shop.gaship.gashipfront.employee.dto.request.EmployeeModifyRequestDto;
+import shop.gaship.gashipfront.employee.dto.response.EmployeeResponseDto;
+import shop.gaship.gashipfront.employee.service.EmployeeService;
+import shop.gaship.gashipfront.util.dto.PageResponse;
+
+/**
+ * 설명작성란
+ *
+ * @author : 유호철
+ * @see EmployeeService
+ * @since 1.0
+ */
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeAdapterImpl employeeAdapter;
+
+    @Override
+    public boolean employeeAdd(EmployeeCreateRequestDto requestDto){
+        return employeeAdapter.employeeAdd(requestDto);
+    }
+
+    @Override
+    public boolean employeeModify(EmployeeModifyRequestDto requestDto){
+        return employeeAdapter.employeeModify(requestDto);
+    }
+
+    @Override
+    public EmployeeResponseDto employeeDetail(Integer employeeNo){
+        return employeeAdapter.employeeDetail(employeeNo);
+    }
+
+    @Override
+    public PageResponse<EmployeeResponseDto> employeeList(Pageable pageable){
+        return employeeAdapter.employeeList(pageable);
+    }
+}
