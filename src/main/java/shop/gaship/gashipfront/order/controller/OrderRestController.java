b7c044f1b10f7c2a37afa7b1726ad930e0a6a6eb
semi <semikimm.m@gmail.com>
2022-08-29 02:44:43 +0900

    [feat✨] : 주문 화면에서의 주문 상품 목록 view 1차 완료

---
+++ src/main/java/shop/gaship/gashipfront/order/controller/OrderRestController.java
@@ -0,0 +1,35 @@
+package shop.gaship.gashipfront.order.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.gaship.gashipfront.cart.dto.response.ProductResponseDto;
+import shop.gaship.gashipfront.cart.service.CartService;
+import shop.gaship.gashipfront.security.basic.dto.SignInSuccessUserDetailsDto;
+import shop.gaship.gashipfront.security.common.dto.UserDetailsDto;
+import java.util.List;
+
+/**
+ * 주문 관련 요청을 처리하기 위한 rest controller 입니다.
+ *
+ * @author : 김세미
+ * @since 1.0
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rest/order")
+public class OrderRestController {
+    private final CartService cartService;
+
+    @GetMapping("/products")
+    public List<ProductResponseDto> orderProductList(
+            @AuthenticationPrincipal UserDetails user) {
+        if(user instanceof UserDetailsDto) {
+            return cartService.getProductsFromCart(((UserDetailsDto) user).getMemberNo().toString());
+        }
+        return cartService.getProductsFromCart(((SignInSuccessUserDetailsDto) user).getMemberNo().toString());
+    }
+}
