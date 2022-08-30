8c87627f7ddd82c68ee4af002a198f1771690129
조재철 <rhsnqk@gmail.com>
2022-08-31 01:37:28 +0900

    [feat✨] : 주문 상품 리스트, 주문 상품 상세 뷰 구현

---
+++ src/main/java/shop/gaship/gashipfront/orderproduct/controller/OrderProductController.java
@@ -0,0 +1,72 @@
+package shop.gaship.gashipfront.orderproduct.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import shop.gaship.gashipfront.orderproduct.dto.response.DeliveryInfoResponseDto;
+import shop.gaship.gashipfront.orderproduct.dto.response.OrderProductDetailResponseDto;
+import shop.gaship.gashipfront.orderproduct.dto.response.OrderProductResponseDto;
+import shop.gaship.gashipfront.orderproduct.service.OrderProductService;
+import shop.gaship.gashipfront.security.common.dto.UserDetailsDto;
+import shop.gaship.gashipfront.util.dto.PageResponse;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+@RequiredArgsConstructor
+@RequestMapping("/member/order-product")
+@Controller
+public class OrderProductController {
+
+    private final OrderProductService orderProductService;
+
+    @GetMapping
+    public String findOrderProductListByMemberNo(@PageableDefault Pageable pageable, Model model,
+        @AuthenticationPrincipal UserDetailsDto userDetailsDto) {
+        PageResponse<OrderProductResponseDto> orderProductResponseDtoPageResponse =
+            orderProductService.findOrderProductListByMemberNo(pageable, userDetailsDto.getMemberNo());
+
+        model.addAttribute("orderProductList", orderProductResponseDtoPageResponse.getContent());
+
+        model.addAttribute("next", orderProductResponseDtoPageResponse.isNext());
+        model.addAttribute("previous", orderProductResponseDtoPageResponse.isPrevious());
+        model.addAttribute("totalPage", orderProductResponseDtoPageResponse.getTotalPages());
+        model.addAttribute("pageNum", orderProductResponseDtoPageResponse.getNumber() + 1);
+        model.addAttribute("previousPageNo", pageable.getPageNumber() - 1);
+        model.addAttribute("nextPageNo", pageable.getPageNumber() + 1);
+
+        model.addAttribute("uri", "/member/order-product");
+
+        return "orderproduct/member/orderProductList";
+    }
+
+    @GetMapping("/{orderProductNo}/details")
+    public String findOrderProductOrderProductNo(@PathVariable(value = "orderProductNum") Integer orderProductNo,
+        @AuthenticationPrincipal UserDetailsDto userDetailsDto, Model model) {
+        OrderProductDetailResponseDto orderProductDetailResponseDto =
+            orderProductService.findOrderProductDetail(orderProductNo, userDetailsDto.getMemberNo());
+
+        model.addAttribute("orderProductDetail", orderProductDetailResponseDto);
+
+        return "orderproduct/member/orderProductDetail";
+    }
+
+    @GetMapping("/delivery-info/tracking-no/{trackingNo}")
+    public String findDeliveryInfoByTrackingNo(@PathVariable(value = "trackingNo") String trackingNo, Model model) {
+        List<DeliveryInfoResponseDto> deliveryInfoResponseDtoList =
+            orderProductService.findDeliveryInfoByTrackingNo(trackingNo);
+
+        model.addAttribute("deliveryInfoList", deliveryInfoResponseDtoList);
+
+        return "orderproduct/member/deliveryInfo";
+    }
+
+}
---
+++ src/main/java/shop/gaship/gashipfront/orderproduct/service/OrderProductService.java
@@ -0,0 +1,22 @@
+package shop.gaship.gashipfront.orderproduct.service;
+
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import shop.gaship.gashipfront.orderproduct.dto.response.DeliveryInfoResponseDto;
+import shop.gaship.gashipfront.orderproduct.dto.response.OrderProductDetailResponseDto;
+import shop.gaship.gashipfront.orderproduct.dto.response.OrderProductResponseDto;
+import shop.gaship.gashipfront.util.dto.PageResponse;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+public interface OrderProductService {
+
+    PageResponse<OrderProductResponseDto> findOrderProductListByMemberNo(
+        Pageable pageable, Integer memberNo);
+
+    List<DeliveryInfoResponseDto> findDeliveryInfoByTrackingNo(String trackingNo);
+
+    OrderProductDetailResponseDto findOrderProductDetail(Integer orderProductNo, Integer memberNo);
+}
