89ed3d62d5d17f037c1b506a6f459d1c5c16468e
조재철 <rhsnqk@gmail.com>
2022-08-30 13:44:24 +0900

    [feat✨] : 페이지 요청시 세션쿠키, 세션 모두 만료시간 초기화 시키는 인터셉터 구현

---
+++ src/main/java/shop/gaship/gashipfront/interceptor/SessionInterceptor.java
@@ -0,0 +1,40 @@
+package shop.gaship.gashipfront.interceptor;
+
+import java.util.Arrays;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import shop.gaship.gashipfront.exceptions.NotFoundSessionIdCookieException;
+
+/**
+ * 요청 마다 세션, 세션 쿠키 타임아웃 초기화 시키기 위한 인터셉터.
+ *
+ * @author : 조재철
+ * @since 1.0
+ */
+@Component
+public class SessionInterceptor implements HandlerInterceptor {
+
+    public static final String SESSION_ID = "GASHIP_SESSIONID";
+    public static final Integer SESSION_AND_COOKIE_TIME_OUT_SECOND = 30 * 60;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            Arrays.stream(cookies)
+                  .filter(cookie -> cookie.getName().equals(SESSION_ID))
+                  .findFirst()
+                  .orElseThrow(NotFoundSessionIdCookieException::new)
+                  .setMaxAge(SESSION_AND_COOKIE_TIME_OUT_SECOND);
+        }
+
+        request.getSession().setMaxInactiveInterval(SESSION_AND_COOKIE_TIME_OUT_SECOND);
+
+        return true;
+    }
+}
