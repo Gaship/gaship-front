d49418e3f7e78516332f6c2b41b1c514c74af101
jun1718 <gbeovhsqhtka@naver.com>
2022-08-31 15:26:17 +0900

    [feat✨] : 고객문의 레이아웃 추가 및 페이징처리추가

--- src/main/java/shop/gaship/gashipfront/inquiry/controller/customer/CustomerInquiryForManagerController.java
+++ src/main/java/shop/gaship/gashipfront/inquiry/controller/customer/CustomerInquiryForManagerController.java
@@ -1,6 +1,9 @@
 package shop.gaship.gashipfront.inquiry.controller.customer;
 
+import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryAttribute.KEY_DETAILS;
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryAttribute.KEY_PAGE_RESPONSE;
+import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_CUSTOMER_INQUIRY_DETAILS;
+import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_CUSTOMER_INQUIRY_DETAILS_ADMIN;
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_CUSTOMER_INQUIRY_LIST;
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_CUSTOMER_INQUIRY_LIST_ADMIN;
 
@@ -10,12 +13,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shop.gaship.gashipfront.inquiry.dto.response.InquiryDetailsResponseDto;
 import shop.gaship.gashipfront.inquiry.dto.response.InquiryListResponseDto;
+import shop.gaship.gashipfront.inquiry.service.common.CommonInquiryService;
 import shop.gaship.gashipfront.inquiry.service.customer.CustomerInquiryService;
+import shop.gaship.gashipfront.inquiry.util.RoleUserMySelfProcessor;
+import shop.gaship.gashipfront.security.common.dto.UserDetailsDto;
 import shop.gaship.gashipfront.util.dto.PageResponse;
 
 /**
@@ -31,6 +40,26 @@ import shop.gaship.gashipfront.util.dto.PageResponse;
 public class CustomerInquiryForManagerController {
 
     private final CustomerInquiryService customerInquiryService;
+    private final CommonInquiryService commonInquiryService;
+
+    /**
+     * 관리자에 대한 고객문의 상세조회 요청을 처리하는 기능입니다.
+     *
+     * @param inquiryNo 조회의 기준이 되는 문의번호입니다.
+     * @return view 경로를 반환합니다.
+     * @author 최겸준
+     */
+    @GetMapping(value = "/customer-inquiries/{inquiryNo}/manager")
+    public String customerInquiryDetails(
+        @PathVariable Integer inquiryNo, Model model) {
+
+        InquiryDetailsResponseDto inquiryDetailsResponseDto =
+            commonInquiryService.findInquiry(inquiryNo);
+
+        model.addAttribute(KEY_DETAILS.getValue(), inquiryDetailsResponseDto);
+        return VIEW_NAME_CUSTOMER_INQUIRY_DETAILS_ADMIN.getValue();
+    }
+
 
     /**
      * 관리자가 모든 고객문의 목록을 최신순으로 조회하는 요청을 처리하는 기능입니다.
@@ -46,6 +75,13 @@ public class CustomerInquiryForManagerController {
         PageResponse<InquiryListResponseDto> pageResponse =
             customerInquiryService.findCustomerInquiries(pageable);
 
+        model.addAttribute("next", pageResponse.isNext());
+        model.addAttribute("previous", pageResponse.isPrevious());
+        model.addAttribute("totalPage", pageResponse.getTotalPages());
+        model.addAttribute("pageNum", pageResponse.getNumber() + 1);
+        model.addAttribute("previousPageNo", pageResponse.getNumber() - 1);
+        model.addAttribute("nextPageNo", pageResponse.getNumber() + 1);
+        model.addAttribute("uri", "/admin/inquiries/customer-inquiries");
         model.addAttribute(KEY_PAGE_RESPONSE.getValue(), pageResponse);
         return VIEW_NAME_CUSTOMER_INQUIRY_LIST_ADMIN.getValue();
     }
