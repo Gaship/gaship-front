d066ee3e4cba091c4bf4d04368192c16fc0ba369
semi <semikimm.m@gmail.com>
2022-09-01 10:53:51 +0900

    [feat✨] : 마이페이지 배송지 관리 (등록, 조회, 삭제) 추가

--- src/main/java/shop/gaship/gashipfront/addresslist/controller/AddressListController.java
+++ src/main/java/shop/gaship/gashipfront/addresslist/controller/AddressListController.java
@@ -3,6 +3,7 @@ package shop.gaship.gashipfront.addresslist.controller;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +11,20 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import shop.gaship.gashipfront.addresslist.dto.request.AddressListAddRequestDto;
 import shop.gaship.gashipfront.addresslist.dto.request.AddressListModifyRequestDto;
 import shop.gaship.gashipfront.addresslist.service.AddressListService;
+import shop.gaship.gashipfront.security.common.dto.UserDetailsDto;
+
+import java.util.Objects;
 
 /**
  * 배송지목록의 컨트롤러입니다.
  *
  * @author 최정우
+ * @author 김세미
  * @since 1.0
  */
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/members/{memberNo}/address-lists")
+@RequestMapping("/member/address-list")
 public class AddressListController {
     private static final String DEFAULT_PATH = "addressLists";
     private final AddressListService addressListService;
@@ -29,15 +34,13 @@ public class AddressListController {
      *
      * @param request 배송지목록등록에 쓰이는 정보입니다.
      * @return 배송지목록 목록을 보여주는 페이지
-     * @author 최정우
+     * @author 최정우, 김세미
      */
     @PostMapping
-    public String addressListAdd(@ModelAttribute @Valid AddressListAddRequestDto request,
-                                 RedirectAttributes redirectAttributes) {
+    public String addressListAdd(@ModelAttribute @Valid AddressListAddRequestDto request) {
         addressListService.addAddressList(request);
-        redirectAttributes.addAttribute("memberNo", request.getMemberNo());
 
-        return "redirect:/members/{memberNo}/address-lists";
+        return "redirect:/member/address-list";
     }
 
     /**
@@ -53,7 +56,7 @@ public class AddressListController {
         addressListService.modifyAddressList(request);
         redirectAttributes.addAttribute("memberNo", request.getMemberNo());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/members/{memberNo}/address-lists";
+        return "redirect:/member/address-lists";
     }
 
     /**
@@ -61,54 +64,54 @@ public class AddressListController {
      *
      * @param addressListNo 수정할 배송지목록의 id 입니다.
      * @return 배송지목록 목록을 보여주는 페이지
-     * @author 최정우
+     * @author 최정우, 김세미
      */
     @DeleteMapping("/{addressListNo}")
-    public String addressListRemove(@PathVariable Integer memberNo,
-                                    @PathVariable Long addressListNo,
-                                    RedirectAttributes redirectAttributes) {
-        redirectAttributes.addAttribute("memberNo", memberNo);
-        redirectAttributes.addAttribute("status", true);
-        addressListService.deleteAddressList(memberNo, addressListNo);
-        return "redirect:/members/{memberNo}/address-lists";
+    public String addressListRemove(@AuthenticationPrincipal UserDetailsDto user,
+                                    @PathVariable Long addressListNo) {
+
+        addressListService.deleteAddressList(user.getMemberNo(), addressListNo);
+        return "redirect:/member/address-list";
     }
 
-    /**
-     * 배송지목록의 상세 페이지 입니다.
-     *
-     * @param addressListNo 조회하려는 배송지목록의 id 입니다.
-     * @param model 모델
-     * @return 배송지목록 상세페이지
-     * @author 최정우
-     */
-    @GetMapping("/{addressListNo}")
-    public String addressListDetails(@PathVariable Integer memberNo,
-                                     @PathVariable Long addressListNo,
-                                     RedirectAttributes redirectAttributes,
-                                     Model model) {
-        redirectAttributes.addAttribute("memberNo", memberNo);
-        redirectAttributes.addAttribute("status", true);
-        model.addAttribute("response", addressListService.findAddressList(memberNo, addressListNo));
-        return "addressListInfo";
-    }
+//    /**
+//     * 배송지목록의 상세 페이지 입니다.
+//     *
+//     * @param addressListNo 조회하려는 배송지목록의 id 입니다.
+//     * @param model 모델
+//     * @return 배송지목록 상세페이지
+//     * @author 최정우
+//     */
+//    @GetMapping("/{addressListNo}")
+//    public String addressListDetails(@PathVariable Integer memberNo,
+//                                     @PathVariable Long addressListNo,
+//                                     RedirectAttributes redirectAttributes,
+//                                     Model model) {
+//        redirectAttributes.addAttribute("memberNo", memberNo);
+//        redirectAttributes.addAttribute("status", true);
+//        model.addAttribute("response", addressListService.findAddressList(memberNo, addressListNo));
+//        return "addressListInfo";
+//    }
 
     /**
-     * 배송지목록을 수정하고 배송지목록으로 돌아갑니다.
+     * 배송지 목록입니다.
      *
-     * @param memberNo 조회하려는 회원 id 입니다.
-     * @param pageable 조회하려는 페이지 번호와 사이즈가 담겨져있는 객체
-     * @param model    모델
-     * @return 배송지목록 목록을 보여주는 페이지
-     * @author 최정우
+     * @param user     현재 로그인한 사용자입니다.
+     * @param pageable 배송지 목록에 보여줄 정보에 대한 페이지 정보입니다.
+     * @return 배송지 목록을 보여주는 페이지 입니다.
+     * @author 김세미
      */
     @GetMapping
-    public String addressLists(@PathVariable Integer memberNo,
+    public String addressLists(@AuthenticationPrincipal UserDetailsDto user,
                                Pageable pageable,
-                               RedirectAttributes redirectAttributes,
                                Model model) {
-        redirectAttributes.addAttribute("memberNo", memberNo);
-        redirectAttributes.addAttribute("status", true);
-        model.addAttribute("response", addressListService.findAddressLists(memberNo, pageable));
-        return DEFAULT_PATH;
+        if(Objects.isNull(user)) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("addressList",
+                addressListService.findAddressLists(user.getMemberNo(), pageable));
+
+        return "addresslist/addressList";
     }
 }
