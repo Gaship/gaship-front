49cb195cecf500854948c74357002a402e15dd66
조재철 <rhsnqk@gmail.com>
2022-09-02 20:09:56 +0900

    [refactor🔨] : dto 수정

--- src/main/java/shop/gaship/gashipfront/delivery/dto/response/DeliveryLocationResponseDto.java
+++ src/main/java/shop/gaship/gashipfront/delivery/dto/response/DeliveryLocationResponseDto.java
@@ -2,11 +2,13 @@ package shop.gaship.gashipfront.delivery.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import lombok.Getter;
 
 /**
  * @author : 조재철
  * @since 1.0
  */
+@Getter
 public class DeliveryLocationResponseDto {
 
     private String status;
