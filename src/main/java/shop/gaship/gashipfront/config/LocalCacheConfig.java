f930b880a36bdedb931c7c7955ead250c903ae62
김보민 <qnt012@naver.com>
2022-09-05 16:07:34 +0900

    [feat✨] : 상품 레디스 캐시로 수정

--- src/main/java/shop/gaship/gashipfront/config/LocalCacheConfig.java
+++ src/main/java/shop/gaship/gashipfront/config/LocalCacheConfig.java
@@ -1,12 +1,22 @@
 package shop.gaship.gashipfront.config;
 
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * cache config.
@@ -21,10 +31,30 @@ public class LocalCacheConfig {
     public static final String PRODUCT_CACHE = "product";
 
     @Bean
+    @Primary
     public CacheManager cacheManager() {
         SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-        simpleCacheManager.setCaches(List.of(new ConcurrentMapCache(CATEGORY_CACHE),
-                new ConcurrentMapCache(PRODUCT_CACHE)));
+        simpleCacheManager.setCaches(List.of(new ConcurrentMapCache(CATEGORY_CACHE)));
         return simpleCacheManager;
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        Map<String, RedisCacheConfiguration> cacheConfiguration = new HashMap<>();
+        cacheConfiguration.put(PRODUCT_CACHE, redisCacheConfiguration.entryTtl(Duration.ofSeconds(180L)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .withInitialCacheConfigurations(cacheConfiguration)
+                .build();
     }
 }
