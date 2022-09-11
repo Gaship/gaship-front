8693faf3e370466a87b523909b32b7791730f02e
oct-sky-out <kms3335k@naver.com>
2022-09-11 15:24:33 +0900

    [feat] : 회원 문의, 상품 문의 접근권한 요청헤더 추가.

--- src/main/java/shop/gaship/gashipfront/inquiry/controller/product/ProductInquiryForManagerController.java
+++ src/main/java/shop/gaship/gashipfront/inquiry/controller/product/ProductInquiryForManagerController.java
@@ -2,17 +2,12 @@ package shop.gaship.gashipfront.inquiry.controller.product;
 
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryAttribute.KEY_DETAILS;
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryAttribute.KEY_PAGE_RESPONSE;
-import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_PRODUCT_INQUIRY_DETAILS;
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_PRODUCT_INQUIRY_DETAILS_ADMIN;
-import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_PRODUCT_INQUIRY_LIST;
 import static shop.gaship.gashipfront.inquiry.inquiryenum.InquiryViewName.VIEW_NAME_PRODUCT_INQUIRY_LIST_ADMIN;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +17,6 @@ import shop.gaship.gashipfront.inquiry.dto.response.InquiryDetailsResponseDto;
 import shop.gaship.gashipfront.inquiry.dto.response.InquiryListResponseDto;
 import shop.gaship.gashipfront.inquiry.service.common.CommonInquiryService;
 import shop.gaship.gashipfront.inquiry.service.product.ProductInquiryService;
-import shop.gaship.gashipfront.inquiry.util.RoleUserMySelfProcessor;
-import shop.gaship.gashipfront.security.common.dto.UserDetailsDto;
 import shop.gaship.gashipfront.util.dto.PageResponse;
 
 /**
